db022228eb2f8cef58e90d72f1449e32483ed7e7
Loïc Poullain <loic.poullain@centraliens.net>
2018-02-14 11:18:09 +0100

    Add multiple views to @foal/examples.

--- packages/examples/src/app/app.module.ts
+++ packages/examples/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { afterThatLog, log, rest, view } from '@foal/common';
+import { afterThatLog, log, rest, view, multipleViews } from '@foal/common';
 import { FoalModule } from '@foal/core';
 
-import { IndexViewService, UserService } from './services';
+import { IndexViewService, PublicViewsService, UserService } from './services';
 
 export const AppModule: FoalModule = {
   controllers: [
+    multipleViews.attachService('/', PublicViewsService),
     view.attachService('/', IndexViewService),
     rest.attachService('/users', UserService),
   ],
--- packages/examples/src/app/services/index.ts
+++ packages/examples/src/app/services/index.ts
@@ -1,3 +1,4 @@
 export { ConnectionService } from './connection.service';
 export { IndexViewService } from './index-view.service';
+export { PublicViewsService } from './public-views.service';
 export { UserService } from './user.service';
