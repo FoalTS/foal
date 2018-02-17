cc7fd773f8c4effc2c3095a51f48e7698c9065aa
Loïc Poullain <loic.poullain@centraliens.net>
2018-02-17 11:16:16 +0100

    In @foal/examples add bootstrap and remove authentication.

--- packages/examples/src/index.ts
+++ packages/examples/src/index.ts
@@ -3,56 +3,20 @@ import { getCallback, handleErrors } from '@foal/express';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as session from 'express-session';
-import * as passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
 
 import { AppModule } from './app/app.module';
-import { UserService } from './app/services/user.service';
+import { authenticate } from './authentication';
 import { config } from './config';
 
 const app = express();
 const foalApp = new Foal(AppModule);
 
-passport.use(new LocalStrategy(
-  async (username, password, done) => {
-    try {
-      const userService = foalApp.services.get(UserService);
-      const users = await userService.getAll({ username });
-      if (users.length === 0 || !userService.verifyPassword(password, users[0].password)) {
-        done(null, false);
-      } else {
-        done(null, users[0]);
-      }
-    } catch (error) {
-      // TODO: express should only return errors on development.
-      done(error);
-    }
-  }
-));
-
-passport.serializeUser((user, cb) => {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(async (id, cb) => {
-  try {
-    const user = await foalApp.services.get(UserService).get(id, {});
-    cb(null, user);
-  } catch (error) {
-    cb(error);
-  }
-});
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'my secret', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// The problem is the failureRedirect -> cannot GET /login
-app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
-  res.send('Authenticated!');
-});
+app.use(authenticate(foalApp));
 
 app.use(getCallback(foalApp));
 
