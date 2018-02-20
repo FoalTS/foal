7b63172949f51ad83eea16be054e72aaf4ab4a44
Loïc Poullain <loic.poullain@centraliens.net>
2018-02-20 19:44:28 +0100

    Fix linting

--- packages/authentication/src/authenticators/local-authenticator.service.spec.ts
+++ packages/authentication/src/authenticators/local-authenticator.service.spec.ts
@@ -1,8 +1,8 @@
 import { ModelService } from '@foal/common';
+import { NotFoundError, ObjectType, UnauthorizedError } from '@foal/core';
 import { expect } from 'chai';
 
-import { LocalAuthenticatorService, CheckPassword } from './local-authenticator.service';
-import { ObjectType, NotFoundError, UnauthorizedError } from '@foal/core';
+import { CheckPassword, LocalAuthenticatorService } from './local-authenticator.service';
 
 describe('LocalAuthenticatorService', () => {
 
@@ -16,36 +16,36 @@ describe('LocalAuthenticatorService', () => {
   let service: ConcreteClass;
 
   class UserModelService implements ModelService<User, ObjectType, ObjectType, any>, CheckPassword<User> {
-    createOne(): any {}
-    createMany(): any {}
-    
-    findById(): any {}
-    findOne(query: ObjectType): User & { id: number } {
-      if (query.email === 'john@gmail.com') {
+    public createOne(): any {}
+    public createMany(): any {}
+
+    public findById(): any {}
+    public findOne(query: ObjectType): User & { id: number } {
+      if (query.email === 'john@foalts.org') {
         return {
+          email: 'john@foalts.org',
           id: 1,
-          username: 'John',
           password: 'strongPassword',
-          email: 'john@gmail.com'
-        }
+          username: 'John',
+        };
       }
       throw new NotFoundError();
     }
-    findAll(): any {}
+    public findAll(): any {}
 
-    findByIdAndUpdate(): any {}
-    findOneAndUpdate(): any {}
-    updateMany(): void {}
+    public findByIdAndUpdate(): any {}
+    public findOneAndUpdate(): any {}
+    public updateMany(): void {}
 
-    findByIdAndReplace(): any {}
-    findOneAndReplace(): any {}
+    public findByIdAndReplace(): any {}
+    public findOneAndReplace(): any {}
 
-    findByIdAndRemove(): any {}
-    findOneAndRemove(): any {}
+    public findByIdAndRemove(): any {}
+    public findOneAndRemove(): any {}
 
-    removeMany(): void {}
-    
-    checkPassword(user: User, password: string): boolean {
+    public removeMany(): void {}
+
+    public checkPassword(user: User, password: string): boolean {
       return user.password === password;
     }
   }
@@ -59,35 +59,35 @@ describe('LocalAuthenticatorService', () => {
 
     it('should throw an UnauthorizedError if no user is found for the given email.', async () => {
       try {
-        await service.authenticate({ email: 'jack@gmail.com', password: 'foo'});
+        await service.authenticate({ email: 'jack@foalts.org', password: 'foo'});
         throw  new Error('No error was thrown in authenticate().');
       } catch (err) {
         expect(err).to.be.instanceOf(UnauthorizedError);
         expect(err.details).to.deep.equal({
           message: 'Incorrect email or password.'
         });
-      };
+      }
     });
 
     it('should throw an UnauthorizedError if the given password is incorrect.', async () => {
       try {
-        await service.authenticate({ email: 'john@gmail.com', password: 'wrongPassword'});
+        await service.authenticate({ email: 'john@foalts.org', password: 'wrongPassword'});
         throw  new Error('No error was thrown in authenticate().');
       } catch (err) {
         expect(err).to.be.instanceOf(UnauthorizedError);
         expect(err.details).to.deep.equal({
           message: 'Incorrect email or password.'
         });
-      };
+      }
     });
 
     it('should return the authenticated user if the given email and password are correct.', async () => {
-      const user = await service.authenticate({ email: 'john@gmail.com', password: 'strongPassword'});
+      const user = await service.authenticate({ email: 'john@foalts.org', password: 'strongPassword'});
       expect(user).to.deep.equal({
+        email: 'john@foalts.org',
         id: 1,
-        username: 'John',
         password: 'strongPassword',
-        email: 'john@gmail.com'
+        username: 'John',
       });
     });
 
