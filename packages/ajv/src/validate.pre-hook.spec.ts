32408ec1fe46e3ab49889f57e3d8550051ef28bf
Loïc Poullain <loic.poullain@centraliens.net>
2018-02-24 11:20:28 +0100

    Udpate @foal/ajv with new architecture.

--- packages/ajv/src/validate.pre-hook.spec.ts
+++ packages/ajv/src/validate.pre-hook.spec.ts
@@ -1,10 +1,10 @@
-import { HttpResponseBadRequest, createEmptyContext, getPreMiddleware, ServiceManager } from '@foal/core';
+import { HttpResponseBadRequest, createEmptyContext, ServiceManager } from '@foal/core';
 import * as Ajv from 'ajv';
 import { expect } from 'chai';
 
 import { validate } from './validate.pre-hook';
 
-describe('validate(schema: ObjectType, ajv = defaultInstance)', () => {
+describe('validate', () => {
 
   it('should not return an HttpResponseBadRequest if ctx.body is validated by ajv for the given schema.', () => {
     const schema = {
@@ -13,13 +13,13 @@ describe('validate(schema: ObjectType, ajv = defaultInstance)', () => {
       },
       type: 'object',
     };
-    const middleware = getPreMiddleware(validate(schema));
+    const hook = validate(schema);
     const ctx = createEmptyContext();
     ctx.body = {
       foo: 3
     };
 
-    const actual = middleware(ctx, new ServiceManager());
+    const actual = hook(ctx, new ServiceManager());
     expect(actual).not.to.be.instanceOf(HttpResponseBadRequest);
   });
 
@@ -30,34 +30,34 @@ describe('validate(schema: ObjectType, ajv = defaultInstance)', () => {
       },
       type: 'object',
     };
-    const middleware = getPreMiddleware(validate(schema));
+    const hook = validate(schema);
     const ctx = createEmptyContext();
 
     function context(body) {
       return { ...ctx, body };
     }
 
-    expect(middleware(context(null), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
-    expect(middleware(context(undefined), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
-    expect(middleware(context('foo'), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
-    expect(middleware(context(3), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
-    expect(middleware(context(true), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
-    expect(middleware(context({ foo: '3' }), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context(null), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context(undefined), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context('foo'), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context(3), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context(true), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
+    expect(hook(context({ foo: '3' }), new ServiceManager())).to.be.instanceOf(HttpResponseBadRequest);
   });
 
-  it('should return an HttpResponseBadRequest with a `details` property if ctx.body is not validated by ajv.', () => {
+  it('should return an HttpResponseBadRequest with a defined `content` property if ctx.body is not validated by ajv.', () => {
     const schema = {
       properties: {
         foo: { type: 'integer' }
       },
       type: 'object',
     };
-    const middleware = getPreMiddleware(validate(schema));
+    const hook = validate(schema);
     const ctx = createEmptyContext();
 
-    const actual = middleware(ctx, new ServiceManager());
+    const actual = hook(ctx, new ServiceManager());
     expect(actual).to.be.instanceOf(HttpResponseBadRequest);
-    expect(actual.details).not.to.equal(undefined);
+    expect((actual as HttpResponseBadRequest).content).not.to.equal(undefined);
   });
 
   it('should use the given ajv instance if it exists.', () => {
@@ -73,12 +73,12 @@ describe('validate(schema: ObjectType, ajv = defaultInstance)', () => {
       foo: 3,
     };
 
-    let middleware = getPreMiddleware(validate(schema));
-    middleware(ctx, new ServiceManager());
+    let hook = validate(schema);
+    hook(ctx, new ServiceManager());
     expect(ctx.body.bar).to.equal(undefined);
 
-    middleware = getPreMiddleware(validate(schema, new Ajv({ useDefaults: true })));
-    middleware(ctx, new ServiceManager());
+    hook = validate(schema, new Ajv({ useDefaults: true }));
+    hook(ctx, new ServiceManager());
     expect(ctx.body.bar).to.equal(4);
   });
 
--- packages/ajv/src/validate.pre-hook.ts
+++ packages/ajv/src/validate.pre-hook.ts
@@ -1,13 +1,13 @@
-import { HttpResponseBadRequest, ObjectType, preHook } from '@foal/core';
+import { Hook, HttpResponseBadRequest, ObjectType } from '@foal/core';
 import * as Ajv from 'ajv';
 
 const defaultInstance = new Ajv();
 
-export function validate(schema: ObjectType, ajv = defaultInstance) {
+export function validate(schema: ObjectType, ajv = defaultInstance): Hook {
   const isValid = ajv.compile(schema);
-  return preHook(ctx => {
+  return ctx => {
     if (!isValid(ctx.body)) {
       return new HttpResponseBadRequest(isValid.errors as Ajv.ErrorObject[]);
     }
-  });
+  };
 }
