39ed4a84bcbd2cc84de8d41d78c7f73986c7787e
Loïc Poullain <loic.poullain@centraliens.net>
2018-03-04 20:38:19 +0100

    Add getAppRouter specs.

--- packages/express/src/get-app-router.spec.ts
+++ packages/express/src/get-app-router.spec.ts
@@ -1,134 +1,146 @@
-// import { expect } from 'chai';
+import { App, basic, HttpMethod, HttpResponseOK } from '@foal/core';
+import * as express from 'express';
+import * as request from 'supertest';
 
-// import { getAppRouter } from './get-app-router';
+import { getAppRouter } from './get-app-router';
+
+function httpMethodTest(httpMethod: HttpMethod) {
+  describe(`when route.httpMethod === "${httpMethod}", route.path === "/foo/bar" and the route returns `
+            + 'an HttpResponseOK(\'Success\')', () => {
+
+    let app;
+
+    beforeEach(() => {
+      app = express();
+      const foalApp = new App({
+        controllers: [
+          basic.attachHandlingFunction(httpMethod, '/foo/bar', ctx => new HttpResponseOK('Success'))
+        ]
+      });
+      app.use(getAppRouter(foalApp));
+      app.use((req, res) => res.sendStatus(404));
+    });
+
+    it(`should respond to ${httpMethod} /foo/bar by 200 "Success".`, () => {
+      switch (httpMethod) {
+        case 'GET':
+          return request(app).get('/foo/bar').expect(200).expect('Success');
+        case 'PATCH':
+          return request(app).patch('/foo/bar').expect(200).expect('Success');
+        case 'PUT':
+          return request(app).put('/foo/bar').expect(200).expect('Success');
+        case 'POST':
+          return request(app).post('/foo/bar').expect(200).expect('Success');
+        case 'DELETE':
+          return request(app).delete('/foo/bar').expect(200).expect('Success');
+      }
+    });
+
+    it(`should respond to ${httpMethod} /foo/bar/ by 200 "Success".`, () => {
+      switch (httpMethod) {
+        case 'GET':
+          return request(app).get('/foo/bar').expect(200).expect('Success');
+        case 'PATCH':
+          return request(app).patch('/foo/bar').expect(200).expect('Success');
+        case 'PUT':
+          return request(app).put('/foo/bar').expect(200).expect('Success');
+        case 'POST':
+          return request(app).post('/foo/bar').expect(200).expect('Success');
+        case 'DELETE':
+          return request(app).delete('/foo/bar').expect(200).expect('Success');
+      }
+    });
+
+    it(`should not respond to ${httpMethod} /foo.`, () => {
+      switch (httpMethod) {
+        case 'GET':
+          return request(app).get('/foo').expect(404);
+        case 'PATCH':
+          return request(app).patch('/foo').expect(404);
+        case 'PUT':
+          return request(app).put('/foo').expect(404);
+        case 'POST':
+          return request(app).post('/foo').expect(404);
+        case 'DELETE':
+          return request(app).delete('/foo').expect(404);
+      }
+    });
+
+    it(`should not respond to ${httpMethod} /foo/bar/4.`, () => {
+      switch (httpMethod) {
+        case 'GET':
+          return request(app).get('/foo/bar/4').expect(404);
+        case 'PATCH':
+          return request(app).patch('/foo/bar/4').expect(404);
+        case 'PUT':
+          return request(app).put('/foo/bar/4').expect(404);
+        case 'POST':
+          return request(app).post('/foo/bar/4').expect(404);
+        case 'DELETE':
+          return request(app).delete('/foo/bar/4').expect(404);
+      }
+    });
+
+    it(`should not respond to [Method !== "${httpMethod}"] /foo/bar.`, () => {
+      const promises: any = [];
+      if (httpMethod !== 'GET') { promises.push(request(app).get('/foo/bar').expect(404)); }
+      if (httpMethod !== 'PATCH') { promises.push(request(app).patch('/foo/bar').expect(404)); }
+      if (httpMethod !== 'PUT') { promises.push(request(app).put('/foo/bar').expect(404)); }
+      if (httpMethod !== 'POST') { promises.push(request(app).post('/foo/bar').expect(404)); }
+      if (httpMethod !== 'DELETE') { promises.push(request(app).delete('/foo/bar').expect(404)); }
+      return Promise.all(promises);
+    });
+
+  });
+}
 
 describe('getAppRouter', () => {
 
   // The below items are more end-to-end tests than unit ones.
 
+  httpMethodTest('DELETE');
+
+  httpMethodTest('GET');
+
+  httpMethodTest('PATCH');
+
+  httpMethodTest('POST');
+
+  httpMethodTest('PUT');
+
+  describe('for each route of each controller of the given app', () => {
+
+    describe('when route.httpMethod === "GET", route.path === "/foo/:id/bar/:id2" and the route returns '
+             + 'an HttpResponseOK(\'Success\')', () => {
+
+      let app;
+
+      beforeEach(() => {
+        app = express();
+        const foalApp = new App({
+          controllers: [
+            basic.attachHandlingFunction('GET', '/foo/:id/bar/:id2', ctx => new HttpResponseOK('Success'))
+          ]
+        });
+        app.use(getAppRouter(foalApp));
+        app.use((req, res) => res.sendStatus(404));
+      });
+
+      it('should respond to GET /foo/12/bar/13 by 200 "Success".', () => {
+        return request(app)
+          .get('/foo/12/bar/13')
+          .expect(200)
+          .expect('Success');
+      });
+
+      it('should not respond to GET /foo/12/bar/.', () => {
+        return request(app)
+          .get('/foo/12/bar/')
+          .expect(404);
+      });
+
+    });
+
+  });
+
 });
-
-  // let route: Route;
-  // let app: any;
-
-  // function getHttpMethodTest(httpMethod: HttpMethod) {
-  //   return () => {
-
-  //     before(() => {
-  //       app = express();
-  //       route = { httpMethod, paths: [], hooks: [], successStatus: 200 };
-  //       app.use(getExpressMiddleware(route));
-  //     });
-
-  //     it(`should return a middleware which responds on ${httpMethod} / .`, () => {
-  //       switch (httpMethod) {
-  //         case 'GET':
-  //           return request(app).get('/').then(res => expect(res.status).not.to.equal(404));
-  //         case 'PATCH':
-  //           return request(app).patch('/').then(res => expect(res.status).not.to.equal(404));
-  //         case 'PUT':
-  //           return request(app).put('/').then(res => expect(res.status).not.to.equal(404));
-  //         case 'POST':
-  //           return request(app).post('/').then(res => expect(res.status).not.to.equal(404));
-  //         case 'DELETE':
-  //           return request(app).delete('/').then(res => expect(res.status).not.to.equal(404));
-  //       }
-  //     });
-
-  //     it(`should return a middleware which does not respond on ${httpMethod} /foobar .`, () => {
-  //       switch (httpMethod) {
-  //         case 'GET':
-  //           return request(app).get('/foobar').expect(404);
-  //         case 'PATCH':
-  //           return request(app).patch('/foobar').expect(404);
-  //         case 'PUT':
-  //           return request(app).put('/foobar').expect(404);
-  //         case 'POST':
-  //           return request(app).post('/foobar').expect(404);
-  //         case 'DELETE':
-  //           return request(app).delete('/foobar').expect(404);
-  //       }
-  //     });
-
-  //     it(`should return a middleware which does not respond on [Method !== "${httpMethod}"] / .`, () => {
-  //       const promises: any = [];
-  //       if (httpMethod !== 'GET') { promises.push(request(app).get('/').expect(404)); }
-  //       if (httpMethod !== 'PATCH') { promises.push(request(app).patch('/').expect(404)); }
-  //       if (httpMethod !== 'PUT') { promises.push(request(app).put('/').expect(404)); }
-  //       if (httpMethod !== 'POST') { promises.push(request(app).post('/').expect(404)); }
-  //       if (httpMethod !== 'DELETE') { promises.push(request(app).delete('/').expect(404)); }
-  //       return Promise.all(promises);
-  //     });
-
-  //   };
-  // }
-
-  // describe('when it is called with { httpMethod: "GET", paths: [] }', getHttpMethodTest('GET'));
-  // describe('when it is called with { httpMethod: "PATCH", paths: [] }', getHttpMethodTest('PATCH'));
-  // describe('when it is called with { httpMethod: "POST", paths: [] }', getHttpMethodTest('POST'));
-  // describe('when it is called with { httpMethod: "PUT", paths: [] }', getHttpMethodTest('PUT'));
-  // describe('when it is called with { httpMethod: "DELETE", paths: [] }', getHttpMethodTest('DELETE'));
-
-  // describe('when it is called with { httpMethod: "GET", paths: [ "/", "/foo", "/", "/", "/bar", "foo" ] }', () => {
-
-  //   before(() => {
-  //     app = express();
-  //     route = {
-  //       hooks: [],
-  //       httpMethod: 'GET',
-  //       paths: [ '/', '/foo', '/', '/', '/bar', 'foo' ],
-  //       successStatus: 200
-  //     };
-  //     app.use(getExpressMiddleware(route));
-  //   });
-
-  //   it('should return a middleware which responds on GET /foo/bar/foo .', () => {
-  //     return request(app)
-  //       .get('/foo/bar/foo')
-  //       .then(res => {
-  //         expect(res.status).not.to.equal(404);
-  //       });
-  //   });
-
-  //   it('should return a middleware which responds on GET /foo/bar/foo/ .', () => {
-  //     return request(app)
-  //       .get('/foo/bar/foo/')
-  //       .then(res => {
-  //         expect(res.status).not.to.equal(404);
-  //       });
-  //   });
-
-  //   it('should return a middleware which does not respond on GET /foo/bar/foo/1 .', () => {
-  //     return request(app)
-  //       .get('/foo/bar/foo/1')
-  //       .expect(404);
-  //   });
-
-  // });
-
-  // describe('when it is called with { httpMethod: "GET", paths: [ "/foo/:id/bar/:id2" ] }', () => {
-
-  //   before(() => {
-  //     app = express();
-  //     route = {
-  //       hooks: [],
-  //       httpMethod: 'GET',
-  //       paths: [ '/foo/:id/bar/:id2' ],
-  //       successStatus: 200
-  //     };
-  //     app.use(getExpressMiddleware(route));
-  //   });
-
-  //   it('should return a middleware which responds on GET /foo/12/bar/13 .', () => {
-  //     return request(app)
-  //       .get('/foo/12/bar/13')
-  //       .expect(404);
-  //   });
-
-  //   it('should return a middleware which does not respond on GET /foo/12/bar/ .', () => {
-  //     return request(app)
-  //       .get('/foo/12/bar/')
-  //       .expect(404);
-  //   });
-
-  // });
--- packages/express/src/get-app-router.ts
+++ packages/express/src/get-app-router.ts
@@ -7,22 +7,21 @@ export function getAppRouter(app: App, stateDef: { req: string, state: string }[
   const router = Router();
   for (const controller of app.controllers) {
     for (const route of controller.getRoutes()) {
-      const path = route.path || '/';
       switch (route.httpMethod) {
         case 'DELETE':
-          router.delete(path, getExpressMiddleware(route, app.services, stateDef));
+          router.delete(route.path, getExpressMiddleware(route, app.services, stateDef));
           break;
         case 'GET':
-          router.get(path, getExpressMiddleware(route, app.services, stateDef));
+          router.get(route.path, getExpressMiddleware(route, app.services, stateDef));
           break;
         case 'PATCH':
-          router.patch(path, getExpressMiddleware(route, app.services, stateDef));
+          router.patch(route.path, getExpressMiddleware(route, app.services, stateDef));
           break;
         case 'POST':
-          router.post(path, getExpressMiddleware(route, app.services, stateDef));
+          router.post(route.path, getExpressMiddleware(route, app.services, stateDef));
           break;
         case 'PUT':
-          router.put(path, getExpressMiddleware(route, app.services, stateDef));
+          router.put(route.path, getExpressMiddleware(route, app.services, stateDef));
           break;
       }
     }
