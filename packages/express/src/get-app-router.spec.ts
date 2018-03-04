4e490f6db74c2ff06fa21a38eef517defdaaf437
Loïc Poullain <loic.poullain@centraliens.net>
2018-03-04 18:10:32 +0100

    Add getExpressMiddleware specs.

--- packages/express/src/get-app-router.spec.ts
+++ packages/express/src/get-app-router.spec.ts
@@ -4,4 +4,131 @@
 
 describe('getAppRouter', () => {
 
+  // The below items are more end-to-end tests than unit ones.
+
 });
+
+  // let route: Route;
+  // let app: any;
+
+  // function getHttpMethodTest(httpMethod: HttpMethod) {
+  //   return () => {
+
+  //     before(() => {
+  //       app = express();
+  //       route = { httpMethod, paths: [], hooks: [], successStatus: 200 };
+  //       app.use(getExpressMiddleware(route));
+  //     });
+
+  //     it(`should return a middleware which responds on ${httpMethod} / .`, () => {
+  //       switch (httpMethod) {
+  //         case 'GET':
+  //           return request(app).get('/').then(res => expect(res.status).not.to.equal(404));
+  //         case 'PATCH':
+  //           return request(app).patch('/').then(res => expect(res.status).not.to.equal(404));
+  //         case 'PUT':
+  //           return request(app).put('/').then(res => expect(res.status).not.to.equal(404));
+  //         case 'POST':
+  //           return request(app).post('/').then(res => expect(res.status).not.to.equal(404));
+  //         case 'DELETE':
+  //           return request(app).delete('/').then(res => expect(res.status).not.to.equal(404));
+  //       }
+  //     });
+
+  //     it(`should return a middleware which does not respond on ${httpMethod} /foobar .`, () => {
+  //       switch (httpMethod) {
+  //         case 'GET':
+  //           return request(app).get('/foobar').expect(404);
+  //         case 'PATCH':
+  //           return request(app).patch('/foobar').expect(404);
+  //         case 'PUT':
+  //           return request(app).put('/foobar').expect(404);
+  //         case 'POST':
+  //           return request(app).post('/foobar').expect(404);
+  //         case 'DELETE':
+  //           return request(app).delete('/foobar').expect(404);
+  //       }
+  //     });
+
+  //     it(`should return a middleware which does not respond on [Method !== "${httpMethod}"] / .`, () => {
+  //       const promises: any = [];
+  //       if (httpMethod !== 'GET') { promises.push(request(app).get('/').expect(404)); }
+  //       if (httpMethod !== 'PATCH') { promises.push(request(app).patch('/').expect(404)); }
+  //       if (httpMethod !== 'PUT') { promises.push(request(app).put('/').expect(404)); }
+  //       if (httpMethod !== 'POST') { promises.push(request(app).post('/').expect(404)); }
+  //       if (httpMethod !== 'DELETE') { promises.push(request(app).delete('/').expect(404)); }
+  //       return Promise.all(promises);
+  //     });
+
+  //   };
+  // }
+
+  // describe('when it is called with { httpMethod: "GET", paths: [] }', getHttpMethodTest('GET'));
+  // describe('when it is called with { httpMethod: "PATCH", paths: [] }', getHttpMethodTest('PATCH'));
+  // describe('when it is called with { httpMethod: "POST", paths: [] }', getHttpMethodTest('POST'));
+  // describe('when it is called with { httpMethod: "PUT", paths: [] }', getHttpMethodTest('PUT'));
+  // describe('when it is called with { httpMethod: "DELETE", paths: [] }', getHttpMethodTest('DELETE'));
+
+  // describe('when it is called with { httpMethod: "GET", paths: [ "/", "/foo", "/", "/", "/bar", "foo" ] }', () => {
+
+  //   before(() => {
+  //     app = express();
+  //     route = {
+  //       hooks: [],
+  //       httpMethod: 'GET',
+  //       paths: [ '/', '/foo', '/', '/', '/bar', 'foo' ],
+  //       successStatus: 200
+  //     };
+  //     app.use(getExpressMiddleware(route));
+  //   });
+
+  //   it('should return a middleware which responds on GET /foo/bar/foo .', () => {
+  //     return request(app)
+  //       .get('/foo/bar/foo')
+  //       .then(res => {
+  //         expect(res.status).not.to.equal(404);
+  //       });
+  //   });
+
+  //   it('should return a middleware which responds on GET /foo/bar/foo/ .', () => {
+  //     return request(app)
+  //       .get('/foo/bar/foo/')
+  //       .then(res => {
+  //         expect(res.status).not.to.equal(404);
+  //       });
+  //   });
+
+  //   it('should return a middleware which does not respond on GET /foo/bar/foo/1 .', () => {
+  //     return request(app)
+  //       .get('/foo/bar/foo/1')
+  //       .expect(404);
+  //   });
+
+  // });
+
+  // describe('when it is called with { httpMethod: "GET", paths: [ "/foo/:id/bar/:id2" ] }', () => {
+
+  //   before(() => {
+  //     app = express();
+  //     route = {
+  //       hooks: [],
+  //       httpMethod: 'GET',
+  //       paths: [ '/foo/:id/bar/:id2' ],
+  //       successStatus: 200
+  //     };
+  //     app.use(getExpressMiddleware(route));
+  //   });
+
+  //   it('should return a middleware which responds on GET /foo/12/bar/13 .', () => {
+  //     return request(app)
+  //       .get('/foo/12/bar/13')
+  //       .expect(404);
+  //   });
+
+  //   it('should return a middleware which does not respond on GET /foo/12/bar/ .', () => {
+  //     return request(app)
+  //       .get('/foo/12/bar/')
+  //       .expect(404);
+  //   });
+
+  // });
--- packages/express/src/get-express-middleware.spec.ts
+++ packages/express/src/get-express-middleware.spec.ts
@@ -1,21 +1,82 @@
-// import {
-//   Context,
-//   HttpMethod,
-//   HttpResponseMethodNotAllowed,
-//   HttpResponseRedirect,
-//   Route
-// } from '@foal/core';
-// import * as bodyParser from 'body-parser';
-// import { expect } from 'chai';
-// import * as express from 'express';
-// import * as request from 'supertest';
+import {
+  HttpResponseOK,
+  Route,
+  ServiceManager
+} from '@foal/core';
+import { expect } from 'chai';
+import * as express from 'express';
+import * as request from 'supertest';
 
-// import { getExpressMiddleware } from './get-express-middleware';
-
-// HACK
-console.error = () => {};
+import { getExpressMiddleware } from './get-express-middleware';
 
 describe(`getExpressMiddleware`, () => {
+
+  // The below items are more end-to-end tests than unit ones.
+
+  it('should get the response for the given route and send it back to the client if it is defined.', () => {
+    const app = express();
+    const route: Route = {
+      handler: ctx => new HttpResponseOK('Success'),
+      httpMethod: 'GET',
+      path: '/',
+      postHooks: [],
+      preHooks: []
+    };
+    app.use(getExpressMiddleware(route, new ServiceManager(), []));
+
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Success');
+  });
+
+  it('should get the response for the given route and not send it back to the client '
+     + 'if it is not defined.', async () => {
+    const app = express();
+    const route: Route = {
+      handler: ctx => {},
+      httpMethod: 'GET',
+      path: '/',
+      postHooks: [],
+      preHooks: []
+    };
+    app.use(getExpressMiddleware(route, new ServiceManager(), []));
+
+    try {
+      await request(app).get('/').timeout(200);
+      throw new Error('The server did not time out.');
+    } catch (err) {
+      if (!err.timeout) {
+        throw err;
+      }
+    }
+  });
+
+  it('should forward the error to the next express middleware if one is thrown or rejected.', () => {
+    const err = Error('Fail');
+    let forwardedError;
+
+    const app = express();
+    const route: Route = {
+      handler: async ctx => { throw err; },
+      httpMethod: 'GET',
+      path: '/',
+      postHooks: [],
+      preHooks: []
+    };
+    app.use(getExpressMiddleware(route, new ServiceManager(), []));
+    app.use((err, req, res, next) => {
+      forwardedError = err;
+      res.send('Caught');
+    });
+
+    return request(app)
+      .get('/')
+      .expect('Caught')
+      .then(() => {
+        expect(forwardedError).to.equal(err);
+      });
+  });
 
   // let route: Route;
   // let app: any;
@@ -110,9 +171,7 @@ describe(`getExpressMiddleware`, () => {
   //   it('should return a middleware which does not respond on GET /foo/bar/foo/1 .', () => {
   //     return request(app)
   //       .get('/foo/bar/foo/1')
-  //       .then(res => {
-  //         expect(res.status).to.equal(404);
-  //       });
+  //       .expect(404);
   //   });
 
   // });
@@ -133,17 +192,13 @@ describe(`getExpressMiddleware`, () => {
   //   it('should return a middleware which responds on GET /foo/12/bar/13 .', () => {
   //     return request(app)
   //       .get('/foo/12/bar/13')
-  //       .then(res => {
-  //         expect(res.status).not.to.equal(404);
-  //       });
+  //       .expect(404);
   //   });
 
   //   it('should return a middleware which does not respond on GET /foo/12/bar/ .', () => {
   //     return request(app)
   //       .get('/foo/12/bar/')
-  //       .then(res => {
-  //         expect(res.status).to.equal(404);
-  //       });
+  //       .expect(404);
   //   });
 
   // });
