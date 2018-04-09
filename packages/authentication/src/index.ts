e134fadb4a826855db75cd132d849ae9de5c6e56
Loïc Poullain <loic.poullain@centraliens.net>
2018-04-09 10:21:49 +0200

    Add validateLocalCredentialsFormat

--- packages/authentication/src/index.ts
+++ packages/authentication/src/index.ts
@@ -1,5 +1,6 @@
 export * from './authenticators';
 export * from './authorization';
+export * from './pre-hooks';
 export * from './authenticate.pre-hook';
 export * from './authentication.controller-factory';
 export * from './authenticator.interface';
