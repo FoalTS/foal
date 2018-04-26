04c6ec8a5a97f3c673501bf23fbfc6ece1a49c86
Loïc Poullain <loic.poullain@centraliens.net>
2018-04-26 12:52:34 +0200

    Improve IServiceControllerFactory test readability.

--- packages/core/src/classes/service-controller-factory.interface.spec.ts
+++ packages/core/src/classes/service-controller-factory.interface.spec.ts
@@ -2,33 +2,39 @@ import { Class } from '../interfaces';
 import { Controller } from './controller';
 import { IServiceControllerFactory } from './service-controller-factory.interface';
 
-interface IService1 {
-  foo();
-}
+describe('IServiceControllerFactory.attachService', () => {
 
-class Service1 implements IService1 {
-  public foo() {}
-}
+  it('should accept optional "options" when it is implemented.', () => {
+    interface IService1 {
+      foo();
+    }
 
-class Foo implements IServiceControllerFactory {
-  public attachService(path: string, ServiceClass: Class<IService1>) {
-    return new Controller(path);
-  }
-}
+    class Service1 implements IService1 {
+      public foo() {}
+    }
 
-class Bar implements IServiceControllerFactory {
-  public attachService(path: string, ServiceClass: Class<IService1>, options: { foobar: string }) {
-    return new Controller(path);
-  }
-}
+    class Foo implements IServiceControllerFactory {
+      public attachService(path: string, ServiceClass: Class<IService1>) {
+        return new Controller(path);
+      }
+    }
 
-class BarFoo implements IServiceControllerFactory {
-  public attachService(path: string, ServiceClass: Class<IService1>, options?: { foobar: string }) {
-    return new Controller(path);
-  }
-}
+    class Bar implements IServiceControllerFactory {
+      public attachService(path: string, ServiceClass: Class<IService1>, options: { foobar: string }) {
+        return new Controller(path);
+      }
+    }
 
-new Foo().attachService('/foo', Service1);
-new Bar().attachService('/foo', Service1, { foobar: 'test' });
-new BarFoo().attachService('/foo', Service1);
-new BarFoo().attachService('/foo', Service1, { foobar: 'test' });
+    class BarFoo implements IServiceControllerFactory {
+      public attachService(path: string, ServiceClass: Class<IService1>, options?: { foobar: string }) {
+        return new Controller(path);
+      }
+    }
+
+    new Foo().attachService('/foo', Service1);
+    new Bar().attachService('/foo', Service1, { foobar: 'test' });
+    new BarFoo().attachService('/foo', Service1);
+    new BarFoo().attachService('/foo', Service1, { foobar: 'test' });
+  });
+
+});
