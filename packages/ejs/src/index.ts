756e41cb3b5b9192e8c44d87c7868f0870a0caea
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-18 18:42:38 +0200

    Remove ejs services and add render utils.

--- packages/ejs/src/index.ts
+++ packages/ejs/src/index.ts
@@ -1,2 +1,11 @@
-export { EjsTemplateService } from './ejs-template.service';
-export { MultipleEjsTemplatesService } from './multiple-ejs-templates.service';
+import { HttpResponseOK } from '@foal/core';
+import { render as renderEjs } from 'ejs';
+
+// Use types.
+export function renderToString(template: string, locals?: object): string {
+  return renderEjs(template, locals);
+}
+
+export function render(template: string, locals?: object): HttpResponseOK {
+  return new HttpResponseOK(renderToString(template, locals));
+}
