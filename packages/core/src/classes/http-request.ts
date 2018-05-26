9c7fd609d041a75c578e180c2fd46251f8b81dc4
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-26 12:59:46 +0200

    Add path and method to HttpRequest.

--- packages/core/src/classes/http-request.ts
+++ packages/core/src/classes/http-request.ts
@@ -1,13 +1,19 @@
+import { HttpMethod } from '../interfaces';
+
 export class HttpRequest {
   params: { [key: string]: any } = {};
   body: any = undefined;
   query: { [key: string]: any } = {};
+  method: HttpMethod = 'GET';
+  path: string = '';
 
   constructor(private expressRequest?) {
     if (expressRequest) {
       this.query = expressRequest.query;
       this.params = expressRequest.params;
       this.body = expressRequest.body;
+      this.method = expressRequest.method;
+      this.path = expressRequest.path;
     }
   }
 
