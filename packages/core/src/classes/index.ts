6e484f94e7db308d0340e4b0752a770c3a01a951
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-27 11:55:49 +0200

    Typo.

--- packages/core/src/classes/index.ts
+++ packages/core/src/classes/index.ts
@@ -1,6 +1,4 @@
 export { App } from './app';
 export { Controller } from './controller';
-export { HttpRequest } from './http-request';
-export * from './http-responses';
 export { Service, ServiceManager } from './service-manager';
 export { Context, PostContext } from './contexts';
