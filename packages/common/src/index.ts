5dd0e465aa5bcb8b2f8978f5271e2be313e2282c
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-27 11:55:05 +0200

    Typo

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,5 +3,3 @@ export * from './post-hooks';
 export * from './pre-hooks';
 export * from './services';
 export * from './utils';
-
-export { isObjectDoesNotExist, ObjectDoesNotExist } from './object-does-not-exist';
