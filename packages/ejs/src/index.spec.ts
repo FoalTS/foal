4ee3c3046f762b4f8ed87bfd8479869a772c01a1
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-27 10:27:57 +0200

    Make @foal/ejs a template engine package.

--- packages/ejs/src/index.spec.ts
+++ packages/ejs/src/index.spec.ts
@@ -1,7 +1,7 @@
 import { HttpResponseOK } from '@foal/core';
 import { expect } from 'chai';
 
-import { render, renderToString } from './index';
+import { renderToString } from './index';
 
 describe('renderToString', () => {
 
@@ -20,31 +20,6 @@ describe('renderToString', () => {
 
   it('should throw an Error if the template and/or locals are incorrect.', () => {
     expect(() => renderToString(template2, {})).to.throw();
-  });
-
-});
-
-describe('render', () => {
-
-  const template1 = 'Hi!';
-  const template2 = 'Hello <%= name %>! How are you?';
-
-  it('should return the ejs template (HttpResponseOK) with no locals if it is correct.', () => {
-    expect(render(template1))
-      .to.be.an.instanceOf(HttpResponseOK)
-      .with.property('content', template1);
-  });
-
-  it('should render the ejs template (HttpResponseOK) with the given locals if it is correct.', () => {
-    const name = 'Foobar';
-    const expected = `Hello ${name}! How are you?`;
-    expect(render(template2, { name }))
-      .to.be.an.instanceOf(HttpResponseOK)
-      .with.property('content', expected);
-  });
-
-  it('should throw an Error if the template and/or locals are incorrect.', () => {
-    expect(() => render(template2, {})).to.throw();
   });
 
 });
--- packages/ejs/src/index.ts
+++ packages/ejs/src/index.ts
@@ -1,17 +1,5 @@
-import { Controller, HttpResponseOK } from '@foal/core';
-import { render as renderEjs } from 'ejs';
+import { render } from 'ejs';
 
-// Use types.
 export function renderToString(template: string, locals?: object): string {
-  return renderEjs(template, locals);
-}
-
-export function render(template: string, locals?: object): HttpResponseOK {
-  return new HttpResponseOK(renderToString(template, locals));
-}
-
-export function view(path: string, template: string, locals?: object): Controller<'main'> {
-  const controller = new Controller<'main'>();
-  controller.addRoute('main', 'GET', path, ctx => render(template, locals));
-  return controller;
+  return render(template, locals);
 }
