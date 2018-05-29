4b4956d9e4f9290585a8cee074ae88fb5d6a9f9f
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-29 10:49:57 +0200

    Remove validateEmailAndPasswordCredentialsFormat.

--- packages/password/src/index.ts
+++ packages/password/src/index.ts
@@ -1,3 +1,2 @@
 export { AbstractEmailAuthenticator } from './email-authenticator.service';
-export { validateEmailAndPasswordCredentialsFormat } from './validate-email-and-password-credentials-format.pre-hook';
 export { parsePassword } from './parse-password.parser';
