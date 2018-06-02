138e2817b00f40101d7809d19879988b410e68ea
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-02 19:31:08 +0200

    Remove generators dir.

--- packages/cli/src/generators/index.ts
+++
@@ -1,5 +0,0 @@
-export { generateApp } from './app';
-export { generateModule } from './module';
-export { generatePostHook } from './post-hook';
-export { generatePreHook } from './pre-hook';
-export { generateService } from './service';
