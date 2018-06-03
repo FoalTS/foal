e07b404612f7cd22f1979b5e14920e24d5ed21a9
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-03 09:56:12 +0200

    Remove webpack from @foal/ejs.

--- packages/ejs/src/index.ts
+++ packages/ejs/src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * FoalTS
+ * Copyright(c) 2017-2018 Loïc Poullain <loic.poullain@centraliens.net>
+ * Released under the MIT License.
+ */
+
 import { render } from 'ejs';
 
 export function renderToString(template: string, locals?: object): string {
