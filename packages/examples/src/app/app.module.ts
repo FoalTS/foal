22c016a86ef0f30de18aa35596a511bcec6aacff
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-03 21:42:11 +0200

    Add get-airport.

--- packages/examples/src/app/app.module.ts
+++ packages/examples/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {
   view,
 } from '@foal/core';
 
+import { getAirport } from './handlers';
 import { User } from './models/user.model';
 import { AuthModule } from './modules/authentication';
 import { UserService } from './services';
@@ -18,23 +19,17 @@ import { UserService } from './services';
 export const AppModule: Module = {
   controllers: [
     rest('/users', UserService)
-      .withPreHooks([
-        ctx => { ctx.request.body.isAdmin = false; },
-      ], 'POST /')
+      .withPreHook(ctx => { ctx.request.body.isAdmin = false; }, 'POST /')
       .withPreHook(restrictAccessToAuthenticated(), 'GET /', 'GET /:id')
       .withPreHook(restrictAccessToAdmin(), 'PUT /:id', 'PATCH /:id', 'DELETE /:id')
       .withPostHook(onSuccessKeepFields<User>([ 'id', 'email', 'roles' ])),
+
     view('/', require('./templates/index.html'), { name: 'FoalTS' }),
+
     view('/home', require('./templates/home.html'))
       .withPreHook(restrictAccessToAuthenticated()),
-    route('GET', '/error', () => {
-        throw new Error('This is an error.');
-      }),
-    route('GET', '/one_user', ctx => {
-      const user = new User();
-      user.roles = [ 'admin' ];
-      return new HttpResponseOK(user);
-    })
+
+    route('GET', '/airport', getAirport),
   ],
   modules: [
     AuthModule,
