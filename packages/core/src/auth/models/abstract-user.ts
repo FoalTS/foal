5e0e5381bcfe432122f90752f673f78cbe129409
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-03 10:14:56 +0200

    Remove webpack from @foal/core.

--- packages/core/src/auth/models/abstract-user.ts
+++ packages/core/src/auth/models/abstract-user.ts
@@ -3,9 +3,11 @@ import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export abstract class AbstractUser extends BaseEntity {
 
   @PrimaryGeneratedColumn()
+  // @ts-ignore : Property 'id' has no initializer and is not definitely assigned in theconstructor.
   id: number;
 
   @Column('simple-array')
+  // @ts-ignore : Property 'roles' has no initializer and is not definitely assigned in theconstructor.
   roles: string[];
 
   hasRole(role: string): boolean {
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * FoalTS
+ * Copyright(c) 2017-2018 Loïc Poullain <loic.poullain@centraliens.net>
+ * Released under the MIT License.
+ */
+
 export * from './auth';
 export * from './common';
 export * from './core';
