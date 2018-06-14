b3589c7027832a69f8690db74620137cb8cd0861
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-14 08:40:54 +0200

    Prepare SQLite and PostgreSQL tests.

--- packages/core/src/common/services/model.service.spec.ts
+++ packages/core/src/common/services/model.service.spec.ts
@@ -341,7 +341,15 @@ describe('ModelService', () => {
   });
   // We'll need to wait for this issue to be fixed before supporting both postgres and sqlite:
   // https://github.com/typeorm/typeorm/issues/1308
-  // testSuite('SQLite', 'sqlite-connection');
-  // testSuite('MariaDB', 'postgres-connection');
+  // testSuite('SQLite', {
+  //   database: './test_db.sqlite',
+  //   type: 'sqlite',
+  // });
+  // testSuite('PostgreSQL', {
+  //   database: 'test',
+  //   password: 'test',
+  //   type: 'postgres',
+  //   username: 'test',
+  // });
 
 });
