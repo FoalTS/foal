d8efcf19b631ed24336f6a311e335258bdbd0920
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-28 16:45:59 +0200

    Remove route controller-factory.

--- packages/core/src/common/controller-factories/route.controller-factory.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Handler, HttpMethod } from '../../core';
-
-export function route(httpMethod: HttpMethod, path: string, handler: Handler): Controller<'main'> {
-  const controller = new Controller<'main'>();
-  controller.addRoute('main', httpMethod, path, handler);
-  return controller;
-}
