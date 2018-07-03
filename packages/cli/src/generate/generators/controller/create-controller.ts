124c61b89dfa933810892b81b9dc31a9b62470c4
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-03 13:13:01 +0200

    Add controller generators.

--- packages/cli/src/generate/generators/controller/create-controller.ts
+++ packages/cli/src/generate/generators/controller/create-controller.ts
@@ -1,3 +1,18 @@
+import { getNames, renderTemplate } from '../../utils';
+
 export function createController({ name, type }: { name: string, type: 'Empty'|'REST'|'GraphQL' }) {
+  const names = getNames(name);
+
+  switch (type) {
+    case 'Empty':
+      renderTemplate('controller/controller.empty.ts', `${names.kebabName}.controller.ts`, names);
+      break;
+    case 'REST':
+      renderTemplate('controller/controller.rest.ts', `${names.kebabName}.controller.ts`, names);
+      break;
+    case 'GraphQL':
+      renderTemplate('controller/controller.graphql.ts', `${names.kebabName}.controller.ts`, names);
+      break;
+  }
 
 }
