130a3c778c992df2616fa4e0dca5b8f31ab3a206
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-04 13:19:55 +0200

    Add sub-folders to module generators.

--- packages/cli/src/generate/generators/module/create-module.spec.ts
+++ packages/cli/src/generate/generators/module/create-module.spec.ts
@@ -7,15 +7,41 @@ import { createModule } from './create-module';
 
 describe('createModule', () => {
 
+  beforeEach(() => createModule({ name: 'test-fooBar', log: false }));
+
   afterEach(() => {
-    rmfileIfExists('test-foo-bar/index.ts');
+    rmfileIfExists('test-foo-bar/controllers/index.ts');
+    rmfileIfExists('test-foo-bar/controllers/test.ts');
+    rmdirIfExists('test-foo-bar/controllers');
+
+    rmfileIfExists('test-foo-bar/hooks/index.ts');
+    rmfileIfExists('test-foo-bar/hooks/test.ts');
+    rmdirIfExists('test-foo-bar/hooks');
+
+    rmfileIfExists('test-foo-bar/entities/index.ts');
+    rmfileIfExists('test-foo-bar/entities/test.ts');
+    rmdirIfExists('test-foo-bar/entities');
+
+    rmfileIfExists('test-foo-bar/sub-modules/index.ts');
+    rmfileIfExists('test-foo-bar/sub-modules/test.ts');
+    rmdirIfExists('test-foo-bar/sub-modules');
+
+    rmfileIfExists('test-foo-bar/services/index.ts');
+    rmfileIfExists('test-foo-bar/services/test.ts');
+    rmdirIfExists('test-foo-bar/services');
+
+    rmfileIfExists('test-foo-bar/templates/index.ts');
+    rmfileIfExists('test-foo-bar/templates/test.ts');
+    rmdirIfExists('test-foo-bar/templates');
+
     rmfileIfExists('test-foo-bar/test-foo-bar.module.ts');
+    rmfileIfExists('test-foo-bar/index.ts');
+    rmfileIfExists('test-foo-bar/test.ts');
+
     rmdirIfExists('test-foo-bar');
   });
 
-  it('should render the templates.', () => {
-
-    createModule({ name: 'test-fooBar' });
+  it('should render the root templates.', () => {
 
     let expected = readFileFromTemplatesSpec('module/index.1.ts');
     let actual = readFileFromRoot('test-foo-bar/index.ts');
@@ -23,6 +49,82 @@ describe('createModule', () => {
 
     expected = readFileFromTemplatesSpec('module/test-foo-bar.module.1.ts');
     actual = readFileFromRoot('test-foo-bar/test-foo-bar.module.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the controllers templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/controllers/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/controllers/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/controllers/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/controllers/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the hooks templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/hooks/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/hooks/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/hooks/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/hooks/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the entities templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/entities/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/entities/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/entities/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/entities/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the sub-modules templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/sub-modules/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/sub-modules/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/sub-modules/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/sub-modules/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the services templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/services/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/services/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/services/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/services/test.ts');
+    expect(actual).to.equal(expected);
+
+  });
+
+  it('should render the templates templates.', () => {
+
+    let expected = readFileFromTemplatesSpec('module/templates/index.1.ts');
+    let actual = readFileFromRoot('test-foo-bar/templates/index.ts');
+    expect(actual).to.equal(expected);
+
+    expected = readFileFromTemplatesSpec('module/templates/test.1.ts');
+    actual = readFileFromRoot('test-foo-bar/templates/test.ts');
     expect(actual).to.equal(expected);
 
   });
--- packages/cli/src/generate/generators/module/create-module.ts
+++ packages/cli/src/generate/generators/module/create-module.ts
@@ -1,10 +1,35 @@
-import { getNames, mkdirIfNotExists, renderTemplate } from '../../utils';
+import { copyFileFromTemplates, getNames, mkdirIfNotExists, renderTemplate } from '../../utils';
 
-export function createModule({ name }: { name: string }) {
+export function createModule({ name, log }: { name: string, log?: boolean }) {
   const names = getNames(name);
 
-  mkdirIfNotExists(names.kebabName);
+  mkdirIfNotExists(names.kebabName, log);
 
-  renderTemplate('module/index.ts', `${names.kebabName}/index.ts`, names);
-  renderTemplate('module/module.ts', `${names.kebabName}/${names.kebabName}.module.ts`, names);
+  renderTemplate('module/index.ts', `${names.kebabName}/index.ts`, names, log);
+  copyFileFromTemplates('module/test.ts', `${names.kebabName}/test.ts`, log);
+  renderTemplate('module/module.ts', `${names.kebabName}/${names.kebabName}.module.ts`, names, log);
+
+  mkdirIfNotExists(`${names.kebabName}/controllers`, log);
+  copyFileFromTemplates('module/controllers/index.ts', `${names.kebabName}/controllers/index.ts`, log);
+  copyFileFromTemplates('module/controllers/test.ts', `${names.kebabName}/controllers/test.ts`, log);
+
+  mkdirIfNotExists(`${names.kebabName}/hooks`, log);
+  copyFileFromTemplates('module/hooks/index.ts', `${names.kebabName}/hooks/index.ts`, log);
+  copyFileFromTemplates('module/hooks/test.ts', `${names.kebabName}/hooks/test.ts`, log);
+
+  mkdirIfNotExists(`${names.kebabName}/entities`, log);
+  copyFileFromTemplates('module/entities/index.ts', `${names.kebabName}/entities/index.ts`, log);
+  copyFileFromTemplates('module/entities/test.ts', `${names.kebabName}/entities/test.ts`, log);
+
+  mkdirIfNotExists(`${names.kebabName}/sub-modules`, log);
+  copyFileFromTemplates('module/sub-modules/index.ts', `${names.kebabName}/sub-modules/index.ts`, log);
+  copyFileFromTemplates('module/sub-modules/test.ts', `${names.kebabName}/sub-modules/test.ts`, log);
+
+  mkdirIfNotExists(`${names.kebabName}/services`, log);
+  copyFileFromTemplates('module/services/index.ts', `${names.kebabName}/services/index.ts`, log);
+  copyFileFromTemplates('module/services/test.ts', `${names.kebabName}/services/test.ts`, log);
+
+  mkdirIfNotExists(`${names.kebabName}/templates`, log);
+  copyFileFromTemplates('module/templates/index.ts', `${names.kebabName}/templates/index.ts`, log);
+  copyFileFromTemplates('module/templates/test.ts', `${names.kebabName}/templates/test.ts`, log);
 }
