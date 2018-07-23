f9208c7ca276a168d4e6bab50b3e820636f510c0
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-23 15:36:05 +0200

    Remove ctx.response

--- packages/core/src/core/http/contexts.ts
+++ packages/core/src/core/http/contexts.ts
@@ -1,10 +1,8 @@
 import { AbstractUser } from '../../auth';
-import { HttpResponse } from './http-responses';
 
 export class Context<User extends AbstractUser = AbstractUser> {
   state: { [key: string]: any } = {};
   user: User|undefined = undefined;
-  response: HttpResponse | undefined = undefined;
 
   constructor(public request) {}
 }
