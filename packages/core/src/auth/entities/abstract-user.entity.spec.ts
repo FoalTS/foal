0ff406f682b6d6cea0a744d921f7aa677aa4aad2
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-23 10:41:48 +0200

    Add AbstractUser tests and implement hasPerm.

--- packages/core/src/auth/entities/abstract-user.entity.spec.ts
+++ packages/core/src/auth/entities/abstract-user.entity.spec.ts
@@ -1,23 +1,114 @@
-import { expect } from 'chai';
-import { BaseEntity } from 'typeorm';
+// 3p
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { BaseEntity, createConnection, Entity, getConnection, getManager } from 'typeorm';
 
+// FoalTS
 import { AbstractUser } from './abstract-user.entity';
+import { Group } from './group.entity';
+import { Permission } from './permission.entity';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
 
 describe('AbstractUser', () => {
 
+  @Entity()
   class User extends AbstractUser {}
 
-  it('should extend BaseEntity', () => {
-    expect(new User()).to.be.an.instanceOf(BaseEntity);
+  beforeEach(() => createConnection({
+    database: 'test',
+    dropSchema: true,
+    entities: [User, Group, Permission],
+    password: 'test',
+    synchronize: true,
+    type: 'mysql',
+    username: 'test',
+  }));
+
+  afterEach(() => getConnection().close());
+
+  it('should have a generated primary key "id".', async () => {
+    const user = new User();
+    user.groups = [];
+    user.userPermissions = [];
+    user.permissions = [];
+    await getManager().save(user);
+    expect(user.id).not.to.equal(undefined);
+  });
+
+  it('should have "permissions" which take Permission instances.', async () => {
+    const permission = new Permission();
+    permission.name = 'permission1';
+    permission.codeName = '';
+    await getManager().save(permission);
+
+    const user = new User();
+    user.permissions = [];
+    user.groups = [];
+    user.userPermissions = [
+      permission
+    ];
+
+    await getManager().save(user);
+
+    const user2 = await getManager().findOne(User, user.id, { relations: ['userPermissions'] });
+    if (!user2) {
+      throw new Error('User should have been saved.');
+    }
+    expect(user2.userPermissions).to.be.an('array').and.to.have.lengthOf(1);
+    expect(user2.userPermissions[0].name).to.equal('permission1');
+  });
+
+  it('should have "groups" which take Group instances.', async () => {
+    const group = new Group();
+    group.name = 'group1';
+    await getManager().save(group);
+
+    const user = new User();
+    user.permissions = [];
+    user.groups = [ group ];
+    user.userPermissions = [];
+
+    await getManager().save(user);
+
+    const user2 = await getManager().findOne(User, user.id, { relations: ['groups'] });
+    if (!user2) {
+      throw new Error('User should have been saved.');
+    }
+    expect(user2.groups).to.be.an('array').and.to.have.lengthOf(1);
+    expect(user2.groups[0].name).to.equal('group1');
   });
 
   describe('when hasPerm is called', () => {
 
-    it('should return true if the user has the given permission and false otherwise.', () => {
+    it('should return true if the user has the given permission (userPermission).', () => {
+      const permission = new Permission();
+      permission.codeName = 'admin';
+
       const user = new User();
-      user.permissions = [ 'admin' ];
+      user.userPermissions = [ permission ];
+
       expect(user.hasPerm('admin')).to.equal(true);
-      expect(user.hasPerm('superuser')).to.equal(false);
+    });
+
+    it('should return true if the user has a group which has the given permission.', () => {
+      const permission = new Permission();
+      permission.codeName = 'admin';
+
+      const group = new Group();
+      group.permissions = [ permission ];
+
+      const user = new User();
+      user.groups = [ group ];
+
+      expect(user.hasPerm('admin')).to.equal(true);
+    });
+
+    it('should return false if neither the user nor its groups have the given permission.', () => {
+      const user = new User();
+
+      expect(user.hasPerm('admin')).to.equal(false);
     });
 
   });
--- packages/core/src/auth/entities/abstract-user.entity.ts
+++ packages/core/src/auth/entities/abstract-user.entity.ts
@@ -1,19 +1,18 @@
 // 3p
-import { BaseEntity, Column, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 // FoalTS
 import { Group } from './group.entity';
 import { Permission } from './permission.entity';
 
-export abstract class AbstractUser extends BaseEntity {
+export abstract class AbstractUser {
 
   @PrimaryGeneratedColumn()
   // @ts-ignore : Property 'id' has no initializer and is not definitely assigned in theconstructor.
   id: number;
 
-  @Column('simple-array')
-  // @ts-ignore : Property 'permissions' has no initializer and is not definitely assigned in theconstructor.
-  permissions: string[];
+  // TODO: remove this
+  permissions: string[] = [];
 
   @ManyToMany(type => Group)
   @JoinTable()
@@ -25,29 +24,21 @@ export abstract class AbstractUser extends BaseEntity {
   // @ts-ignore : Property 'userPermissions' has no initializer and is not definitely assigned in theconstructor.
   userPermissions: Permission[];
 
-  @Column({ default: false })
-  // @ts-ignore : Property 'isSuperUser' has no initializer and is not definitely assigned in theconstructor.
-  isSuperUser: boolean;
-
-  getGroupPermissions(): string[] {
-    // SELECT permission.codeName from Permission
-    // Where (permission.id = permGroup.permId AND permGroup.groupID = userGroup.groupId AND userGroup.userId = :id)
-    //  OR (permission.id = permUser.permId AND permUser.userId = :id)
-    // const user = await AbstractUser.createQueryBuilder('user')
-    //   .innerJoin('user.groups', 'group')
-    //   .innerJoin('group.permissions', 'permission')
-    //   .where('user.id = :id', { id: 1 })
-    //   .getOne();
-    // return user.permissions;
-    return [];
-  }
-
-  getAllPermissions(): string[] {
-    return this.permissions;
-  }
-
-  hasPerm(perm: string): boolean {
-    return this.getAllPermissions().includes(perm);
+  hasPerm(codeName: string): boolean {
+    // return this.permissions.includes(codeName);
+    for (const permission of this.userPermissions || []) {
+      if (permission.codeName === codeName) {
+        return true;
+      }
+    }
+    for (const group of this.groups || []) {
+      for (const permission of group.permissions || []) {
+        if (permission.codeName === codeName) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
 }
