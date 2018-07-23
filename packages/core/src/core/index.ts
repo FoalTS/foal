98bb5bb49dc350c3ba2a744d77ac1906d0ca35a0
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-23 23:18:19 +0200

    Remove old controller

--- packages/core/src/core/index.ts
+++ packages/core/src/core/index.ts
@@ -4,6 +4,5 @@ export * from './http';
 export * from './hooks';
 export * from './routes';
 export { Config } from './config';
-export { Controller as Controller2 } from './controller';
 export * from './modules';
 export { Service, ServiceManager } from './service-manager';
