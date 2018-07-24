1ebf43274c4c66ce49a979d0eed817884ab4f0c6
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-24 16:22:33 +0200

    [@foal/cli] Add smart detection folder with foal g service.

--- packages/cli/src/generate/generators/service/create-service.spec.ts
+++ packages/cli/src/generate/generators/service/create-service.spec.ts
@@ -2,24 +2,69 @@
 import { expect } from 'chai';
 
 // FoalTS
-import { readFileFromRoot, readFileFromTemplatesSpec, rmfileIfExists } from '../../utils';
+import {
+  mkdirIfNotExists,
+  readFileFromRoot,
+  readFileFromTemplatesSpec,
+  rmdirIfExists,
+  rmfileIfExists
+} from '../../utils';
 import { createService } from './create-service';
 
 describe('createService', () => {
 
   afterEach(() => {
+    rmfileIfExists('src/app/services/test-foo-bar.service.ts');
+    rmdirIfExists('src/app/services');
+    rmdirIfExists('src/app');
+    // We cannot remove src/ since the generator code lives within. This is bad testing
+    // approach.
+    // rmdirIfExists('src');
+
+    rmfileIfExists('services/test-foo-bar.service.ts');
+    rmdirIfExists('services');
+
     rmfileIfExists('test-foo-bar.service.ts');
+
     rmfileIfExists('test-foo-bar-serializer.service.ts');
     rmfileIfExists('test-foo-bar-resolver.service.ts');
   });
 
-  it('should render the empty templates.', () => {
+  describe('should render the empty templates.', () => {
 
-    createService({ name: 'test-fooBar', type: 'Empty' });
+    it('in src/app/services/ if the directory exists.', () => {
+      mkdirIfNotExists('src', false);
+      mkdirIfNotExists('src/app', false);
+      mkdirIfNotExists('src/app/services', false);
 
-    const expected = readFileFromTemplatesSpec('service/test-foo-bar.service.empty.ts');
-    const actual = readFileFromRoot('test-foo-bar.service.ts');
-    expect(actual).to.equal(expected);
+      createService({ name: 'test-fooBar', type: 'Empty' });
+
+      const expected = readFileFromTemplatesSpec('service/test-foo-bar.service.empty.ts');
+      const actual = readFileFromRoot('src/app/services/test-foo-bar.service.ts');
+      expect(actual).to.equal(expected);
+
+    });
+
+    it('in services/ if the directory exists.', () => {
+      mkdirIfNotExists('services', false);
+
+      createService({ name: 'test-fooBar', type: 'Empty' });
+
+      const expected = readFileFromTemplatesSpec('service/test-foo-bar.service.empty.ts');
+      const actual = readFileFromRoot('services/test-foo-bar.service.ts');
+      expect(actual).to.equal(expected);
+
+    });
+
+    it('in the current directory otherwise.', () => {
+
+      createService({ name: 'test-fooBar', type: 'Empty' });
+
+      const expected = readFileFromTemplatesSpec('service/test-foo-bar.service.empty.ts');
+      const actual = readFileFromRoot('test-foo-bar.service.ts');
+      expect(actual).to.equal(expected);
+
+    });
 
   });
 
