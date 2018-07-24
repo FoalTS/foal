61cbac2bbe788200910e642d3bf9f468f45de181
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-24 16:08:43 +0200

    [@foal/cli] Add smart detection folder with foal g entity.

--- packages/cli/src/generate/generators/entity/create-entity.ts
+++ packages/cli/src/generate/generators/entity/create-entity.ts
@@ -1,7 +1,17 @@
+import { existsSync } from 'fs';
+
 import { getNames, renderTemplate } from '../../utils';
 
 export function createEntity({ name }: { name: string }) {
   const names = getNames(name);
 
-  renderTemplate('entity/entity.ts', `${names.kebabName}.entity.ts`, names);
+  let path = `${names.kebabName}.entity.ts`;
+
+  if (existsSync('src/app/entities')) {
+    path = `src/app/entities/${path}`;
+  } else if (existsSync('entities')) {
+    path = `entities/${path}`;
+  }
+
+  renderTemplate('entity/entity.ts', path, names);
 }
