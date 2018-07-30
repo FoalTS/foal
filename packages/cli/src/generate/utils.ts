e3a3c9e195f535c45b1f9289b257617cb02ba9d9
Loïc Poullain <loic.poullain@centraliens.net>
2018-07-30 08:21:00 +0200

    Fix camel/kebab case issue.

--- packages/cli/src/generate/utils.ts
+++ packages/cli/src/generate/utils.ts
@@ -64,7 +64,7 @@ export function readFileFromRoot(src: string): string {
 }
 
 export function getNames(name: string): { camelName: string, kebabName: string, upperFirstCamelName: string } {
-  const camelName = name.replace(/-([a-z])/i, g => g[1].toUpperCase());
+  const camelName = name.replace(/-([a-z])/gi, g => g[1].toUpperCase());
   const kebabName = name.replace(/([a-z][A-Z])/g, g => `${g[0]}-${g[1].toLowerCase()}`);
   const upperFirstCamelName = camelName.charAt(0).toUpperCase() + camelName.slice(1);
 
