169865c298f541e6add46f3b25500064c73da567
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-10 22:28:24 +0200

    Complete auth e2e tests.

--- packages/core/e2e/auth.spec.ts
+++ packages/core/e2e/auth.spec.ts
@@ -2,6 +2,7 @@
 import * as request from 'supertest';
 
 // FoalTS
+import { ok } from 'assert';
 import { Column, createConnection, Entity, getConnection, getRepository } from '../node_modules/typeorm';
 import {
   AbstractUser,
@@ -14,9 +15,11 @@ import {
   Group,
   HttpResponseOK,
   IModule,
+  InitDB,
   LoginController,
   LoginRequired,
   Module,
+  parsePassword,
   Permission,
   PermissionRequired,
   Service,
@@ -63,6 +66,7 @@ it('Authentication and authorization', async () => {
   }
 
   @Module()
+  @InitDB([ User, Permission, Group ])
   @Authenticate(User)
   class AppModule implements IModule {
     controllers = [
@@ -72,6 +76,8 @@ it('Authentication and authorization', async () => {
   }
 
   const app = createApp(AppModule);
+
+  /* Create a user */
 
   await createConnection({
     database: 'e2e_db.sqlite',
@@ -84,30 +90,95 @@ it('Authentication and authorization', async () => {
 
   const user = new User();
   user.email = 'john@foalts.org';
-  user.password = 'password';
+  user.password = await parsePassword('password');
   await getRepository(User, 'create-connection').save(user);
 
   await getConnection('create-connection').close();
+
+  /* Try to access routes that require authentication and a specific permission */
 
   await Promise.all([
     request(app).get('/foo').expect(401),
     request(app).get('/bar').expect(401),
   ]);
 
+  /* Try to login with a wrong email */
+
   await request(app)
     .post('/login')
     .send({ email: 'jack@foalts.org', password: 'password' })
-    .end((err, data) => console.log(err, data));
-    // .expect(401);
+    .expect(401);
+
+  /* Try to login with a wrong password */
 
   await request(app)
     .post('/login')
     .send({ email: 'john@foalts.org', password: 'wrong-password' })
     .expect(401);
 
+  /* Log in */
+
+  let cookie = '';
   await request(app)
     .post('/login')
     .send({ email: 'john@foalts.org', password: 'password' })
-    .expect(201);
+    .expect(204)
+    .then(data => {
+      ok(Array.isArray(data.header['set-cookie']));
+      cookie = data.header['set-cookie'][0];
+    });
+
+  /* Access and try to access routes that require authentication and a specific permission */
+
+  await Promise.all([
+    request(app).get('/foo').set('Cookie', cookie).expect(200),
+    request(app).get('/bar').set('Cookie', cookie).expect(403),
+  ]);
+
+  /* Add the admin group and permission */
+
+  await createConnection({
+    database: 'e2e_db.sqlite',
+    entities: [ User, Permission, Group ],
+    name: 'perm-connection',
+    type: 'sqlite',
+  });
+
+  const user2 = await getRepository(User, 'perm-connection').findOne({ email: 'john@foalts.org' });
+  if (!user2) {
+    throw new Error('John was not found in the database.');
+  }
+
+  const perm = new Permission();
+  perm.codeName = 'admin';
+  perm.name = 'Admin permission';
+  await getRepository(Permission, 'perm-connection').save(perm);
+
+  const group = new Group();
+  group.name = 'Administrators';
+  group.permissions = [ perm ];
+  await getRepository(Group, 'perm-connection').save(group);
+
+  user2.groups = [ group ];
+  await getRepository(User, 'perm-connection').save(user2);
+
+  await getConnection('perm-connection').close();
+
+  /* Access the route that require a specific permission */
+
+  await request(app).get('/bar').set('Cookie', cookie).expect(200);
+
+  /* Log out */
+
+  await request(app).get('/logout').set('Cookie', cookie).expect(204);
+
+  /* Try to access routes that require authentication and a specific permission */
+
+  await Promise.all([
+    request(app).get('/foo').expect(401),
+    request(app).get('/bar').expect(401),
+  ]);
 
 });
+
+after(() => getConnection().close());
