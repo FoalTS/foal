c883660a831d69c58624dd6897317db58ac8aa3c
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-10 12:28:28 +0200

    Add createController.

--- packages/core/src/core/controllers.spec.ts
+++ packages/core/src/core/controllers.spec.ts
@@ -1,11 +1,12 @@
 // std
-import { strictEqual } from 'assert';
+import { ok, strictEqual } from 'assert';
 
 // 3p
 import 'reflect-metadata';
 
 // FoalTS
-import { Controller } from './controllers';
+import { Controller, createController } from './controllers';
+import { Service } from './service-manager';
 
 describe('Controller', () => {
 
@@ -15,6 +16,49 @@ describe('Controller', () => {
 
     const actual = Reflect.getOwnMetadata('path', Foobar);
     strictEqual(actual, '/foo');
+  });
+
+});
+
+describe('createController', () => {
+
+  it('should instantiate a controller (no dependencies).', () => {
+    @Controller()
+    class MyController {
+
+    }
+    const controller = createController(MyController);
+    ok(controller instanceof MyController);
+  });
+
+  it('should instantiate a controller with its dependencies.', () => {
+    @Service()
+    class MyService {}
+
+    @Controller()
+    class MyController {
+      constructor(public myService: MyService) {}
+    }
+    const controller = createController(MyController);
+    ok(controller instanceof MyController);
+    ok(controller.myService instanceof MyService);
+  });
+
+  it('should instantiate an inherited controller with its dependencies.', () => {
+    @Service()
+    class MyService {}
+
+    @Controller()
+    class MyController {
+      constructor(public myService: MyService) {}
+    }
+
+    @Controller()
+    class ChildController extends MyController {}
+
+    const controller = createController(ChildController);
+    ok(controller instanceof ChildController);
+    ok(controller.myService instanceof MyService);
   });
 
 });
--- packages/core/src/core/controllers.ts
+++ packages/core/src/core/controllers.ts
@@ -1,6 +1,19 @@
 // 3p
 import 'reflect-metadata';
 
+// FoalTS
+import { Class } from './class.interface';
+import { getMetadata } from './routes/utils';
+import { ServiceManager } from './service-manager';
+
 export function Controller(path?: string) {
   return Reflect.metadata('path', path);
+}
+
+export function createController<T>(controllerClass: Class<T>): T {
+  const controllerDependencies = getMetadata('design:paramtypes', controllerClass) as Class[] || [];
+  const services = new ServiceManager();
+  return new controllerClass(
+    ...controllerDependencies.map(serviceClass => services.get(serviceClass))
+  );
 }
