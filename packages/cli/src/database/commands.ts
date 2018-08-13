34974b01f64466f60bf04a52d3ac22501c65bd2a
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-13 15:20:00 +0200

    Add createpermission and creategroup commands.

--- packages/cli/src/database/commands.ts
+++ packages/cli/src/database/commands.ts
@@ -2,7 +2,32 @@
 import * as program from 'commander';
 
 // FoalTS
+import { createGroup } from './create-group';
+import { createPermission } from './create-permission';
 import { createUser } from './create-user';
+
+program
+  .command('createpermission <name> <codeName>')
+  .description('Creates a new permission in the default database.')
+  .action((name: string, codeName: string) => {
+    console.log(
+      createPermission(name, codeName)
+    );
+  });
+
+program
+  .command('creategroup <name> <codeName>')
+  .description('Creates a new user group in the default database.')
+  .option(
+    '-p, --permissions <permissions>',
+    'Code names of the group permissions (separated by commas with no spaces).'
+  )
+  .action((name: string, codeName: string, options) => {
+    const permissions = (options.permissions as string || '').split(',').filter(s => s !== '');
+    console.log(
+      createGroup(name, codeName, permissions)
+    );
+  });
 
 program
   .command('createuser')
@@ -10,20 +35,22 @@ program
   .option('-g, --groups <groups>', 'Code names of the groups of the user (separated by commas with no spaces).')
   .option(
     '-u, --user-permissions <permissions>',
-    'Code names of the permissions of the user (separated by commas with no spaces).'
+    'Code names of the user permissions (separated by commas with no spaces).'
   )
   .option('-p, --properties <mode>', 'A JSON object which defines the other properties of the user.')
   .action(options => {
-    const groups = (options.groups || '').split(',');
-    const userPermissions = (options.userPermissions || '').split(',');
+    const groups = (options.groups as string || '').split(',').filter(s => s !== '');
+    const userPermissions = (options.userPermissions as string || '').split(',').filter(s => s !== '');
     let properties: object;
     try {
-      properties = JSON.parse(options.properties || '{}');
+      properties = JSON.parse(options.properties as string || '{}');
     } catch (err) {
-      console.error('An error happened while parsing the "properties" json object. Make sure that is'
+      console.log('An error happened while parsing the "properties" json object. Make sure that is'
         + ' not a "quote" issue.');
-      console.error(err.message);
+      console.log(err.message);
       return;
     }
-    createUser(groups, userPermissions, properties);
+    console.log(
+      createUser(groups, userPermissions, properties)
+    );
   });
