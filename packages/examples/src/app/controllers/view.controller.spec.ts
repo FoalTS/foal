3e72d3e6814059b74cfe8a9ba8ed5035318af22c
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-21 17:48:50 +0200

    [@foal/examples] Move scripts to src/ and update package commands

--- packages/examples/src/app/controllers/view.controller.spec.ts
+++ packages/examples/src/app/controllers/view.controller.spec.ts
@@ -9,20 +9,20 @@ import { ViewController } from './view.controller';
 
 describe('ViewController', () => {
 
-  describe('should has an "index" method that', () => {
+  describe('should has an "home" method that', () => {
 
     it('should handle requests at GET /.', () => {
-      strictEqual(getHttpMethod(ViewController, 'index'), 'GET');
-      strictEqual(getPath(ViewController, 'index'), '/');
+      strictEqual(getHttpMethod(ViewController, 'home'), 'GET');
+      strictEqual(getPath(ViewController, 'home'), '/');
     });
 
   });
 
-  describe('should has an "home" method that', () => {
+  describe('should has an "admin" method that', () => {
 
-    it('should handle requests at GET /home.', () => {
-      strictEqual(getHttpMethod(ViewController, 'home'), 'GET');
-      strictEqual(getPath(ViewController, 'home'), '/home');
+    it('should handle requests at GET /admin.', () => {
+      strictEqual(getHttpMethod(ViewController, 'admin'), 'GET');
+      strictEqual(getPath(ViewController, 'admin'), '/admin');
     });
 
   });
---
+++ packages/examples/src/index.spec.ts
@@ -0,0 +1,3 @@
+it('A test', () => {
+  console.log('This is a test');
+});
