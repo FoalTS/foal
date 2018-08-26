93c5b35482f0cd10122ee64ac01ed5e6cb61ff04
Loïc Poullain <loic.poullain@centraliens.net>
2018-08-26 14:07:32 +0200

    [@foal/cli] Move utils to utils/

--- packages/cli/src/generate/utils/index.ts
+++ packages/cli/src/generate/utils/index.ts
@@ -33,7 +33,7 @@ export function renderTemplate(src: string, dest: string, locals: object) {
   if (process.env.NODE_ENV !== 'test') {
     console.log(`CREATE ${dest}`);
   }
-  const template = fs.readFileSync(join(__dirname, './templates', src), 'utf8');
+  const template = fs.readFileSync(join(__dirname, '../templates', src), 'utf8');
   let content = template;
   for (const key in locals) {
     if (locals.hasOwnProperty(key)) {
@@ -58,22 +58,22 @@ export function copyFileFromNodeModules(src: string, dest: string) {
   if (process.env.NODE_ENV !== 'test') {
     console.log(`CREATE ${dest}`);
   }
-  fs.copyFileSync(join(__dirname, '../../node_modules', src), dest);
+  fs.copyFileSync(join(__dirname, '../../../node_modules', src), dest);
 }
 
 export function copyFileFromTemplates(src: string, dest: string) {
   if (process.env.NODE_ENV !== 'test') {
     console.log(`CREATE ${dest}`);
   }
-  fs.copyFileSync(join(__dirname, './templates', src), dest);
+  fs.copyFileSync(join(__dirname, '../templates', src), dest);
 }
 
 export function readFileFromTemplatesSpec(src: string): string {
-  return fs.readFileSync(join(__dirname, './templates-spec', src), 'utf8');
+  return fs.readFileSync(join(__dirname, '../templates-spec', src), 'utf8');
 }
 
 export function readFileFromNodeModules(src: string): string {
-  return fs.readFileSync(join(__dirname, '../../node_modules', src), 'utf8');
+  return fs.readFileSync(join(__dirname, '../../../node_modules', src), 'utf8');
 }
 
 export function readFileFromRoot(src: string): string {
