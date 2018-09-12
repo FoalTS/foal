caddabb795c557468e520e11c0d84f48774ad48c
Loïc Poullain <loic.poullain@centraliens.net>
2018-09-12 10:04:37 +0200

    [@foal/password] Implement isCommon

--- packages/password/src/is-common.util.ts
+++ packages/password/src/is-common.util.ts
@@ -1,3 +1,15 @@
-export function isCommon(password: string): boolean {
-  return false;
+// std
+import { readFile } from 'fs';
+import { join } from 'path';
+import { promisify } from 'util';
+import { gunzip } from 'zlib';
+
+let list: string[];
+
+export async function isCommon(password: string): Promise<boolean> {
+  if (!list) {
+    const fileContent = await promisify(readFile)(join(__dirname, './10-million-password-list-top-10000.txt.gz'));
+    list =  (await promisify(gunzip)(fileContent)).toString().split('\n');
+  }
+  return list.includes(password);
 }
