d22d31689f76daff595eaf32e274a8316c3553fd
Loïc Poullain <loic.poullain@centraliens.net>
2018-09-14 16:54:54 +0200

    Remove getCommandLineArguments from @foal/core.

--- packages/core/src/common/utils/index.ts
+++ packages/core/src/common/utils/index.ts
@@ -1,7 +1,6 @@
 export { controller } from './controller.util';
 export { escapeProp } from './escape-prop';
 export { escape } from './escape';
-export { getCommandLineArguments } from './get-command-line-arguments.util';
 export { render } from './render.util';
 export { subModule } from './sub-module.util';
 export { validate } from './validate.util';
