5e4f3fe7907958a3e59c3d7fccd010f6cdd34825
Loïc Poullain <loic.poullain@centraliens.net>
2018-09-14 19:41:29 +0200

    Add ValidateHeaders and ValidateParams

---
+++ packages/core/src/common/hooks/validate-headers.hook.ts
@@ -0,0 +1,19 @@
+import * as Ajv from 'ajv';
+
+import { Hook, HookDecorator, HttpResponseBadRequest } from '../../core';
+import { getAjvInstance } from '../utils/get-ajv-instance';
+
+/**
+ * Hook to validate the headers of the request.
+ *
+ * @param schema Schema used to validate the headers request.
+ */
+export function ValidateHeaders(schema: object): HookDecorator {
+  const ajv = getAjvInstance();
+  const isValid = ajv.compile(schema);
+  return Hook(ctx => {
+    if (!isValid(ctx.request.headers)) {
+      return new HttpResponseBadRequest(isValid.errors as Ajv.ErrorObject[]);
+    }
+  });
+}
