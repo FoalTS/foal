2ee0577157998317d092b556b15a353a1591d6d1
Loïc Poullain <loic.poullain@centraliens.net>
2018-09-18 10:47:20 +0200

    Remove makeModuleRoutes.

--- packages/core/src/core/routes/index.ts
+++ packages/core/src/core/routes/index.ts
@@ -1,4 +1,3 @@
 export * from './route.interface';
 export { makeControllerRoutes } from './make-controller-routes';
-export { makeModuleRoutes } from './make-module-routes';
 export { getPath, getHttpMethod } from './utils';
