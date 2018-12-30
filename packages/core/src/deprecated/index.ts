ba859a7102eb4259494387cc113f3037b21f3916
Loïc Poullain <loic.poullain@centraliens.net>
2018-12-30 19:40:26 +0100

    Remove parsePassword

--- packages/core/src/deprecated/index.ts
+++ packages/core/src/deprecated/index.ts
@@ -1,5 +1,4 @@
 export { IAuthenticator } from './authenticator.interface';
 export { LoginController, strategy, Strategy } from './login.controller';
-export { parsePassword } from './parse-password.parser';
 export { IResourceCollection, CollectionParams } from './resource-collection.interface';
 export { RestController } from './rest.controller';
