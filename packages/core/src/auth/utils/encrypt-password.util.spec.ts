475a12cd7cc85b7ef08912e8cce38d2c14c3c46d
Loïc Poullain <loic.poullain@centraliens.net>
2018-12-30 17:43:24 +0100

    Implement encryptPassword

---
+++ packages/core/src/auth/utils/encrypt-password.util.spec.ts
@@ -0,0 +1,40 @@
+// std
+import { strictEqual } from 'assert';
+import { pbkdf2Sync } from 'crypto';
+
+// FoalTS
+import { encryptPassword } from './encrypt-password.util';
+
+describe('encryptPassword', () => {
+
+  it('should encrypt the plain password into a 32-byte derived key with PBKDF2/SHA256,'
+      + ' 150 000 iterations and a 16-byte random salt.', async () => {
+    const plainPassword = 'hello world';
+    const actual = await encryptPassword(plainPassword);
+
+    const [ algorithm, iterations, salt, derivedKey ] = actual.split('$');
+
+    strictEqual(algorithm, 'pbkdf2_sha256');
+    strictEqual(parseInt(iterations, 10), 150000);
+    strictEqual(Buffer.from(salt, 'base64').length, 16);
+    strictEqual(Buffer.from(derivedKey, 'base64').length, 32);
+
+    const expectedBuffer = await pbkdf2Sync(plainPassword, Buffer.from(salt, 'base64'), 150000, 32, 'sha256');
+
+    strictEqual(derivedKey, expectedBuffer.toString('base64'));
+  });
+
+  it('should encode the derived key in hexadecimal if options.encoding = "hex".', async () => {
+    const plainPassword = 'hello world';
+    const actual = await encryptPassword(plainPassword, { encoding: 'hex' });
+
+    const [ algorithm, iterations, salt, derivedKey ] = actual.split('$');
+
+    const expectedBuffer = await pbkdf2Sync(
+      plainPassword, Buffer.from(salt, 'hex'), parseInt(iterations, 10), 32, 'sha256'
+    );
+
+    strictEqual(derivedKey, expectedBuffer.toString('hex'));
+  });
+
+});
--- packages/core/src/auth/utils/encrypt-password.util.ts
+++ packages/core/src/auth/utils/encrypt-password.util.ts
@@ -1,3 +1,15 @@
-export function encryptPassword(plainTextPassword: string): string {
-  return '';
+import { pbkdf2, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+export async function encryptPassword(plainTextPassword: string,
+                                      options: { encoding?: 'hex'|'base64' } = {}): Promise<string> {
+  const saltBuffer = await promisify(randomBytes)(16);
+  const iterations = 150000;
+  const keylen = 32;
+  const digest = 'sha256';
+  const derivedKeyBuffer = await promisify(pbkdf2)(plainTextPassword, saltBuffer, iterations, keylen, digest);
+
+  const salt = saltBuffer.toString(options.encoding || 'base64');
+  const derivedKey = derivedKeyBuffer.toString(options.encoding || 'base64');
+  return `pbkdf2_${digest}$${iterations}$${salt}$${derivedKey}`;
 }
