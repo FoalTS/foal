f4a64b0aea5f25911f4daece10e823dbe061078d
Loïc Poullain <loic.poullain@centraliens.net>
2019-01-03 22:17:37 +0100

    [@foal/typeorm] Add missing export

--- packages/typeorm/src/deprecated/index.ts
+++ packages/typeorm/src/deprecated/index.ts
@@ -1,3 +1,3 @@
 export { EmailUser, EmailAuthenticator } from './email-authenticator.service';
 export { emailSchema } from './email.schema';
-export { Middleware, RelationLoader, EntityResourceCollection } from './entity-resource-collection.service';
+export { middleware, Middleware, RelationLoader, EntityResourceCollection } from './entity-resource-collection.service';
