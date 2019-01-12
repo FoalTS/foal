8221b9e9067e587decc8a523c23d496c94a06127
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-01-12 13:42:07 +0530

    validate arguments for createapp command :construction:
    fix linting errors :construction:
    
    Add newline at EOF :tada:

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,6 +6,7 @@
  */
 
 // 3p
+import chalk from 'chalk';
 import * as program from 'commander';
 
 // FoalTS
@@ -24,6 +25,7 @@ import { runScript } from './run-script';
 
 // tslint:disable-next-line:no-var-requires
 const pkg = require('../package.json');
+const args = process.argv.slice(3);
 
 program
   .version(pkg.version, '-v, --version');
@@ -32,6 +34,10 @@ program
   .command('createapp <name>')
   .description('Creates a new directory with a new FoalTS app.')
   .action((name: string) => {
+    if (args.length > 1) {
+      console.log(chalk.red.bold('\n Kindly provide only one argument as the project name'));
+      process.exit(1);
+    }
     createApp({ name, autoInstall: true });
   });
 
