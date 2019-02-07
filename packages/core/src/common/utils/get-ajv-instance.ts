c139980747d1975cba56f230f9bb0d201ea5dad4
Loïc Poullain <loic.poullain@centraliens.net>
2019-02-07 10:18:49 +0100

    [Docs] Document getAjvInstance function

--- packages/core/src/common/utils/get-ajv-instance.ts
+++ packages/core/src/common/utils/get-ajv-instance.ts
@@ -8,14 +8,22 @@ export const _instanceWrapper: { instance: null|Ajv.Ajv } = {
   instance: null
 };
 
+/**
+ * Returns the Ajv instance used internally by FoalTS.
+ *
+ * It has this default configuration:
+ *  - coerceTypes: true (Change data type of data to match `type` keyword.)
+ *  - removeAdditional: true (Remove additional properties when `additionalProperties` keyword is false.)
+ *  - useDefaults: true (Replace missing properties and items with the values from corresponding `default` keyword)
+ *
+ * This configuration can be overrided using the file `config/ajv.json` or through environment
+ * variables: AJV_COERCE_TYPES, AJV_REMOVE_ADDITIONAL, USE_DEFAULTS.
+ */
 export function getAjvInstance(): Ajv.Ajv {
   if (!_instanceWrapper.instance) {
     _instanceWrapper.instance = new Ajv({
-      // change data type of data to match type keyword
       coerceTypes: Config.get('ajv', 'coerceTypes', true) as boolean,
-      // remove additional properties
       removeAdditional: Config.get('ajv', 'removeAdditional', true) as boolean,
-      // replace missing properties and items with the values from corresponding default keyword
       useDefaults: Config.get('ajv', 'useDefaults', true) as boolean,
     });
   }
