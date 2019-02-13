e25f9d2915ac9f45b7d6c17e163fa4b7a34339e8
Loïc Poullain <loic.poullain@centraliens.net>
2019-02-13 13:48:14 +0100

    Simplify config system and add YAML support

--- packages/core/src/core/config.spec.ts
+++ packages/core/src/core/config.spec.ts
@@ -1,21 +1,27 @@
 // std
-import { strictEqual, throws } from 'assert';
-import { join } from 'path';
+import { strict, strictEqual } from 'assert';
 
 // FoalTS
+import { existsSync, mkdirSync, rmdirSync, unlinkSync, writeFileSync } from 'fs';
 import { Config } from './config';
+import { createService } from './service-manager';
+
+function removeFile(path: string) {
+  if (existsSync(path)) {
+    unlinkSync(path);
+  }
+}
+
+// Compilation tests (types)
+// const anyConfig = Config.get('');
+// const stringConfig = Config.get<string>('', 'default value');
+// const stringConfig2 = Config.get<string|undefined>('');
 
 describe('Config', () => {
 
-  let initialRoot: string;
   let initialEnv: string|undefined;
 
-  before(() => {
-    initialEnv = process.env.NODE_ENV;
-    initialRoot = Config.root;
-    Config.root = join(__dirname, './config.spec');
-  });
-  after(() => Config.root = initialRoot);
+  before(() => initialEnv = process.env.NODE_ENV);
 
   afterEach(() => {
     if (initialEnv === undefined) {
@@ -24,91 +30,215 @@ describe('Config', () => {
     } else {
       process.env.NODE_ENV = initialEnv;
     }
+    delete process.env.BAR_FOO;
+    delete process.env.TEST_FOO_FOO_BAR;
+    delete process.env.TEST_FOO_FOO_BAR1;
+    delete process.env.TEST_FOO_FOO_BAR2;
+    delete process.env.TEST_FOO_FOO_BAR3;
+    delete process.env.DB_USERNAME;
+
+    removeFile('.env');
+    removeFile('config/default.json');
+    removeFile('config/default.yml');
+    removeFile('config/test.json');
+    removeFile('config/test.yml');
+    removeFile('config/development.json');
+    removeFile('config/development.yml');
+
+    if (existsSync('config')) {
+      rmdirSync('config');
+    }
   });
 
-  describe('when get is called', () => {
+  describe('when get is called (static)', () => {
 
-    describe('if there is a matching environment variable', () => {
-
-      afterEach(() => {
-        delete process.env.FOO_BAR_BARFOO;
-      });
-
-      it('should return it (string).', () => {
-        process.env.FOO_BAR_BARFOO = 'foo';
-        strictEqual(Config.get('Foo', 'barBarfoo'), 'foo');
-      });
-
-      it('should return it (boolean).', () => {
-        process.env.FOO_BAR_BARFOO = 'true';
-        strictEqual(Config.get('Foo', 'barBarfoo'), true);
-        process.env.FOO_BAR_BARFOO = 'false';
-        strictEqual(Config.get('Foo', 'barBarfoo'), false);
-      });
-
-      it('should return it (number).', () => {
-        process.env.FOO_BAR_BARFOO = '15';
-        strictEqual(Config.get('Foo', 'barBarfoo'), 15);
-      });
-
+    it('should return the value of the environment variable if it exists.', () => {
+      process.env.TEST_FOO_FOO_BAR = 'value1';
+      strictEqual(Config.get('test.foo.fooBar'), 'value1');
     });
 
-    describe('if there is a matching property in a json file (env)', () => {
+    it('should return the value of the .env file if it exists (LF).', () => {
+      const fileContent = 'DB_HOST=localhost\nSETTINGS_SESSION_NAME=id\nFOO_BAR=3\n';
+      writeFileSync('.env', fileContent, 'utf8');
 
-      it('should return it.', () => {
-        delete process.env.NODE_ENV;
-        strictEqual(Config.get('a', 'foo'), 1);
-        process.env.NODE_ENV = 'production';
-        strictEqual(Config.get('a', 'foo'), 2);
+      strictEqual(Config.get('settings.sessionName'), 'id');
+    });
+
+    it('should return the value of the .env file if it exists (CRLF).', () => {
+      const fileContent = 'DB_HOST=localhost\r\nSETTINGS_SESSION_NAME=id\r\nFOO_BAR=3\n';
+      writeFileSync('.env', fileContent, 'utf8');
+
+      strictEqual(Config.get('settings.sessionName'), 'id');
+    });
+
+    it('should return the value of the config/default.json file if it exists.', () => {
+      const fileContent = JSON.stringify({
+        jwt: { subSection: { secretOrPublicKey: 'xxx' } }
       });
+      mkdirSync('config');
+      writeFileSync('config/default.json', fileContent, 'utf8');
 
-      it('should throw an Error it if its type is neither a number, a string nor a boolean.', () => {
-        process.env.NODE_ENV = 'development';
-        throws(
-          () => Config.get('a', 'bar'),
-          err => err.message === 'Config: only string, number and boolean values are supported.'
-            + ' bar type is "object" in a.development.json.'
-        );
+      strictEqual(Config.get('jwt.subSection.secretOrPublicKey'), 'xxx');
+    });
+
+    it('should return the value of the config/default.yml file if it exists.', () => {
+      const fileContent = 'aa:\n  subSection:\n    wx: y\n';
+      mkdirSync('config');
+      writeFileSync('config/default.yml', fileContent, 'utf8');
+
+      strictEqual(Config.get('aa.subSection.wx'), 'y');
+    });
+
+    it('should return the value of the config/${env}.json file if it exists.', () => {
+      process.env.NODE_ENV = 'test';
+      const fileContent = JSON.stringify({
+        auth: { subSection: { key1: 'aaa' } }
       });
+      mkdirSync('config');
+      writeFileSync('config/test.json', fileContent, 'utf8');
 
+      strictEqual(Config.get('auth.subSection.key1'), 'aaa');
     });
 
-    describe('if there is a matching property in a json file', () => {
+    it('should return the value of the config/{env}.yml file if it exists.', () => {
+      process.env.NODE_ENV = 'test';
+      const fileContent = 'hh:\n  subSection:\n    au: ji\n';
+      mkdirSync('config');
+      writeFileSync('config/test.yml', fileContent, 'utf8');
 
-      it('should return it.', () => {
-        strictEqual(Config.get('b', 'bar'), true);
+      strictEqual(Config.get('hh.subSection.au'), 'ji');
+    });
+
+    it('should return undefined if the key does not exist and if no default value is provided.', () => {
+      strictEqual(Config.get('aa.bbbCcc.y'), undefined);
+    });
+
+    it('should return the default value if the key does not exist.', () => {
+      strictEqual(Config.get('aa.bbbCcc.y', false), false);
+    });
+
+    it('should parse environment variable values.', () => {
+      process.env.TEST_FOO_FOO_BAR1 = 'true';
+      strictEqual(Config.get('test.foo.fooBar1'), true);
+
+      process.env.TEST_FOO_FOO_BAR2 = 'false';
+      strictEqual(Config.get('test.foo.fooBar2'), false);
+
+      process.env.TEST_FOO_FOO_BAR3 = '36';
+      strictEqual(Config.get('test.foo.fooBar3'), 36);
+    });
+
+    it('should parse .env values.', () => {
+      const fileContent = 'FOO_BAR1=true\nFOO_BAR2=false\r\nFOO_BAR3=42\n';
+      writeFileSync('.env', fileContent, 'utf8');
+
+      strictEqual(Config.get('foo.bar1'), true);
+      strictEqual(Config.get('foo.bar2'), false);
+      strictEqual(Config.get('foo.bar3'), 42);
+    });
+
+    it('should use "development" as default environment if none is specified.', () => {
+      const fileContent = JSON.stringify({
+        a: 'b'
       });
+      mkdirSync('config');
+      writeFileSync('config/development.json', fileContent, 'utf8');
 
-      it('should not return it if its type is neither a number, a string nor a boolean.', () => {
-        throws(
-          () => Config.get('b', 'barfoo'),
-          err => err.message === 'Config: only string, number and boolean values are supported.'
-            + ' barfoo type is "object" in b.json.'
-        );
+      const ymlFileContent = 'c: d';
+      writeFileSync('config/development.yml', ymlFileContent, 'utf8');
+
+      strictEqual(Config.get('a'), 'b');
+      strictEqual(Config.get('c'), 'd');
+    });
+
+    it('should look at the different values / files in the correct order.', () => {
+      process.env.NODE_ENV = 'test';
+      mkdirSync('config');
+
+      const dotEnvFileContent = 'BAR_FOO=foo2';
+      const defaultJSONFileContent = JSON.stringify({ barFoo: 'foo3' });
+      const defaultYAMLFileContent = 'barFoo: foo4';
+      const envJSONFileContent = JSON.stringify({ barFoo: 'foo5' });
+      const envYAMLFileContent = 'barFoo: foo6';
+
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo7');
+
+      writeFileSync('config/test.yml', envYAMLFileContent, 'utf8');
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo6');
+
+      writeFileSync('config/test.json', envJSONFileContent, 'utf8');
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo5');
+
+      writeFileSync('config/default.yml', defaultYAMLFileContent, 'utf8');
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo4');
+
+      writeFileSync('config/default.json', defaultJSONFileContent, 'utf8');
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo3');
+
+      writeFileSync('.env', dotEnvFileContent, 'utf8');
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo2');
+
+      process.env.BAR_FOO = 'foo1';
+      strictEqual(Config.get('barFoo', 'foo7'), 'foo1');
+    });
+
+    it('should not take too long.', () => {
+      process.env.NODE_ENV = 'test';
+      process.env.DB_USERNAME = 'test';
+      mkdirSync('config');
+
+      const dotEnvFileContent = 'DB_PASSWORD=foo';
+      writeFileSync('.env', dotEnvFileContent, 'utf8');
+      const defaultJSONFileContent = JSON.stringify({ barFoo: 'foo3' });
+      writeFileSync('config/default.json', defaultJSONFileContent, 'utf8');
+      const defaultYAMLFileContent = 'barFoo: foo4';
+      writeFileSync('config/default.yml', defaultYAMLFileContent, 'utf8');
+      const envJSONFileContent = JSON.stringify({
+        jwt: {
+          secretOrPublicKey: 'kljdsqjheblajubdqsmk'
+        },
+        settings: {
+          csrf: false,
+          debug: false,
+          loggerFormat: 'tiny',
+          port: 3000,
+          sessionResave: false,
+          sessionSaveUninitialized: false,
+          sessionSecret: '79120183c32f87b25fbe0da73426dcca',
+          staticUrl: 'public/',
+        }
       });
+      writeFileSync('config/test.json', envJSONFileContent, 'utf8');
+      const envYAMLFileContent = `app:
+    port: 8080
+    baseUrl: 'http://localhost'
+auth:
+    alg: 'RSA'
+api:
+    authService: http://auth-service:8080
+    someUrl: '/blah'`;
+      writeFileSync('config/test.yml', envYAMLFileContent, 'utf8');
 
+      function testResponseTime(key: string) {
+        const NS_PER_SEC = 1e9;
+        delete require.cache[require.resolve('yamljs')];
+        const time = process.hrtime();
+        Config.get(key);
+        const diff = process.hrtime(time);
+        strictEqual(diff[0], 0);
+        strictEqual(diff[1] < 3e6, true, `Expected Config.get to be executed in less than 1ms. Took ${diff[1]} ns.`);
+      }
+
+      testResponseTime('barFoo');
+      testResponseTime('settings.sessionSaveUninitialized');
+      testResponseTime('auth.alg');
     });
 
-    it('should return undefined if there is no matching property or environment variable and '
-        + 'if no default option is provided.' , () => {
-      strictEqual(Config.get('b', 'foo'), undefined);
-      strictEqual(Config.get('c', 'foo'), undefined);
-    });
+  });
 
-    it('should return a default value if one is provided and there is no matching property '
-        + 'or environment variable and if no default option is provided.', () => {
-      strictEqual(Config.get('b', 'foo', 46), 46);
-      strictEqual(Config.get('c', 'foo', 46), 46);
-    });
-
-    it('should handle properly overriding in case there are several matching properties and/or '
-        + 'environment variable.', () => {
-      process.env.D_FOO = '1';
-      strictEqual(Config.get('d', 'foo'), 1);
-      strictEqual(Config.get('d', 'bar'), 2);
-      delete process.env.D_FOO;
-    });
-
+  it('should accept to be used as a service.', () => {
+    const service = createService(Config);
+    strictEqual(service.get('foo', 3), 3);
   });
 
 });
--- packages/core/src/core/config.ts
+++ packages/core/src/core/config.ts
@@ -1,73 +1,141 @@
-import * as fs from 'fs';
-import * as path from 'path';
+// 3p
+import { existsSync, readFileSync } from 'fs';
 
 export class Config {
 
-  static root = path.join(process.cwd(), 'config');
+  static get<T = any>(key: string, defaultValue?: T): T {
+    const underscoreName = this.dotToUnderscore(key);
 
-  static get(configName: string, propName: string,
-             defaultValue?: number | string | boolean): number | string | boolean | undefined {
-    const value = Config.getEnvironmentVariable(configName, propName);
-    if (value !== undefined) {
-      return value;
+    const envValue = process.env[underscoreName];
+    if (envValue !== undefined) {
+      return this.convertType(envValue) as any;
     }
-    const environment = process.env.NODE_ENV || 'development';
-    const value2 = Config.getJsonVariable(configName, propName, environment);
-    if (value2 !== undefined) {
-      return value2;
+
+    const dotEnvValue = this.readDotEnvValue(underscoreName);
+    if (dotEnvValue !== undefined) {
+      return dotEnvValue as any;
     }
-    const value3 = Config.getJsonVariable(configName, propName);
-    if (value3 !== undefined) {
-      return value3;
+
+    const defaultJSONValue = this.readJSONValue('config/default.json', key);
+    if (defaultJSONValue !== undefined) {
+      return defaultJSONValue;
     }
-    return defaultValue;
+
+    const defaultYAMLValue = this.readYAMLValue('config/default.yml', key);
+    if (defaultYAMLValue !== undefined) {
+      return defaultYAMLValue;
+    }
+
+    const envJSONFilePath = `config/${process.env.NODE_ENV || 'development'}.json`;
+    const envJSONValue = this.readJSONValue(envJSONFilePath, key);
+    if (envJSONValue !== undefined) {
+      return envJSONValue;
+    }
+
+    const envYamlFilePath = `config/${process.env.NODE_ENV || 'development'}.yml`;
+    const envYAMLValue = this.readYAMLValue(envYamlFilePath, key);
+    if (envYAMLValue !== undefined) {
+      return envYAMLValue;
+    }
+
+    return defaultValue as T;
   }
 
-  private static getEnvironmentVariable(configName: string, propName: string): string | number | boolean | undefined {
-    const name = `${Config.toUnderscoreUpperCase(configName)}_${Config.toUnderscoreUpperCase(propName)}`;
-    const config = process.env[name];
-    if (config === undefined) {
-      return undefined;
-    }
-    if (config === 'true') {
-      return true;
-    }
-    if (config === 'false') {
-      return false;
-    }
-    const num = parseInt(config, 10);
-    if (!isNaN(num)) {
-      return num;
-    }
-    return config;
-  }
+  private static yaml;
 
-  private static getJsonVariable(configName: string, propName: string, env?: string):
-      string | number | boolean | undefined {
-    const filename = env ? `${configName}.${env}.json` : `${configName}.json`;
-    const filePath = path.join(Config.root, filename);
-
-    if (!fs.existsSync(filePath)) {
+  private static readDotEnvValue(name: string): string | boolean | number | undefined {
+    if (!existsSync('.env')) {
       return;
     }
-    const config = require(filePath);
 
-    const propType = typeof config[propName];
-    if (!['string', 'number', 'boolean', 'undefined'].includes(propType)) {
-      throw new Error(
-        'Config: only string, number and boolean values are supported.'
-        + ` ${propName} type is "${propType}" in ${filename}.`
-      );
+    const envFileContent = readFileSync('.env', 'utf8');
+    const config = {};
+    envFileContent.replace(/\r\n/g, '\n').split('\n').forEach(line => {
+      const [ key, value ] = line.split('=');
+      config[key] = value;
+    });
+
+    if (config[name] !== undefined) {
+      return this.convertType(config[name]);
     }
-
-    return config[propName];
   }
 
-  private static toUnderscoreUpperCase(camelCase: string): string {
-    let result = camelCase.replace(/([A-Z])/g, letter => `_${letter}`);
-    if (result.startsWith('_')) {
-      result = result.substring(1);
+  private static readJSONValue(path: string, key: string): any {
+    if (!existsSync(path)) {
+      return;
     }
-    return result.toUpperCase();
+
+    const fileContent = readFileSync(path, 'utf8');
+    const config = JSON.parse(fileContent);
+    return this.getValue(config, key);
   }
+
+  private static readYAMLValue(path: string, key: string): any {
+    if (!existsSync(path)) {
+      return;
+    }
+
+    const yaml = this.getYAMLInstance();
+    if (!yaml) {
+      console.log(`Impossible to read ${path}. The package "yamljs" is not installed.`);
+      return;
+    }
+
+    const fileContent = readFileSync(path, 'utf8');
+    const config = yaml.parse(fileContent);
+    return this.getValue(config, key);
+  }
+
+  private static getYAMLInstance(): false | any {
+    if (this.yaml === false) {
+      return false;
+    }
+    try {
+      this.yaml = require('yamljs');
+    } catch (err) {
+      if (err.code !== 'MODULE_NOT_FOUND') {
+        throw err;
+      }
+      this.yaml = false;
+    }
+    return this.yaml;
+  }
+
+  private static dotToUnderscore(str: string): string {
+    return str
+      .replace(/([A-Z])/g, letter => `_${letter}`)
+      .replace(/\./g, '_')
+      .toUpperCase();
+  }
+
+  private static convertType(value: string): boolean | number | string {
+    if (value === 'true') {
+      return true;
+    }
+    if (value === 'false') {
+      return false;
+    }
+    const n = parseInt(value, 10);
+    if (!isNaN(n)) {
+      return n;
+    }
+    return value;
+  }
+
+  private static getValue(config: object, propertyPath: string): any {
+    const properties = propertyPath.split('.');
+    let result = config;
+    for (const property of properties) {
+      result = result[property];
+      if (result === undefined) {
+        break;
+      }
+    }
+    return result;
+  }
+
+  get<T = any>(key: string, defaultValue?: T): T {
+    return Config.get<T>(key, defaultValue);
+  }
+
 }
