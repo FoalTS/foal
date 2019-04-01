135efdd1b07e65b20601c8275015ccf33891f9ec
Loïc Poullain <loic.poullain@centraliens.net>
2019-04-01 17:57:42 +0200

    [@foal/swagger] Add swagger UI

---
+++ packages/swagger/src/swagger-controller.spec.ts
@@ -0,0 +1,195 @@
+// std
+import { ok, strictEqual } from 'assert';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { Readable } from 'stream';
+
+// 3p
+import { getHttpMethod, getPath, isHttpResponseOK } from '@foal/core';
+
+// FoalTS
+import { SwaggerController } from './swagger-controller';
+
+function streamToBuffer(stream: Readable): Promise<Buffer> {
+  const chunks: Buffer[] = [];
+  return new Promise<Buffer>((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+}
+
+describe('SwaggerController', () => {
+
+  class ConcreteClass extends SwaggerController {
+    options = { url: 'foobar' };
+  }
+
+  /* Spec file(s) */
+
+  /* UI */
+
+  describe('has a "index" method that', () => {
+
+    it('should handle requests at GET /.', () => {
+      strictEqual(getHttpMethod(ConcreteClass, 'index'), 'GET');
+      strictEqual(getPath(ConcreteClass, 'index'), '/');
+    });
+
+    // TODO: Test the redirect
+
+    it('should properly render the template given options are { url: "xxx" }.', async () => {
+      class ConcreteClass extends SwaggerController {
+        options = { url: 'xxx' };
+      }
+      const controller = new ConcreteClass();
+      const response = await controller.index();
+
+      if (!isHttpResponseOK(response)) {
+        throw new Error('SwaggerController.index should return an HttpResponseOK instance.');
+      }
+
+      strictEqual(response.getHeader('Content-Type'), 'text/html; charset=utf-8');
+
+      const expected = readFileSync(join(__dirname, 'index.url.spec.html'), 'utf8');
+      strictEqual(response.body, expected);
+    });
+
+    it('should properly render the template given options are AppController.', async () => {
+      class ConcreteClass extends SwaggerController {
+        options = { controllerClass: class {} };
+      }
+      const controller = new ConcreteClass();
+      const response = await controller.index();
+
+      if (!isHttpResponseOK(response)) {
+        throw new Error('SwaggerController.index should return an HttpResponseOK instance.');
+      }
+
+      strictEqual(response.getHeader('Content-Type'), 'text/html; charset=utf-8');
+
+      const expected = readFileSync(join(__dirname, 'index.controller.spec.html'), 'utf8');
+      strictEqual(response.body, expected);
+    });
+
+    it('should properly render the template given options are [{ url: "v1.json", name: "v1" }, '
+      + '{ controllerClass: AppController, name: "v2" }].', async () => {
+        class ConcreteClass extends SwaggerController {
+          options = [
+            {  name: 'v1', url: 'v1.json' },
+            {  name: 'v2', controllerClass: class {} },
+          ];
+        }
+        const controller = new ConcreteClass();
+        const response = await controller.index();
+
+        if (!isHttpResponseOK(response)) {
+          throw new Error('SwaggerController.index should return an HttpResponseOK instance.');
+        }
+
+        strictEqual(response.getHeader('Content-Type'), 'text/html; charset=utf-8');
+
+        const expected = readFileSync(join(__dirname, 'index.no-primary.spec.html'), 'utf8');
+        strictEqual(response.body, expected);
+    });
+
+    it('should properly render the template given options are [{ url: "xxx", name: "spec1" }, '
+      + '{ url: "yyy", name: "spec2", primary: true }].', async () => {
+        class ConcreteClass extends SwaggerController {
+          options = [
+            {  name: 'spec1', url: 'xxx' },
+            {  name: 'spec2', url: 'yyy', primary: true },
+          ];
+        }
+        const controller = new ConcreteClass();
+        const response = await controller.index();
+
+        if (!isHttpResponseOK(response)) {
+          throw new Error('SwaggerController.index should return an HttpResponseOK instance.');
+        }
+
+        strictEqual(response.getHeader('Content-Type'), 'text/html; charset=utf-8');
+
+        const expected = readFileSync(join(__dirname, 'index.primary.spec.html'), 'utf8');
+        strictEqual(response.body, expected);
+
+    });
+
+  });
+
+  describe('has a "swaggerUi" method that', () => {
+
+    it('should handle requests at GET /.', () => {
+      strictEqual(getHttpMethod(ConcreteClass, 'swaggerUi'), 'GET');
+      strictEqual(getPath(ConcreteClass, 'swaggerUi'), '/swagger-ui.css');
+    });
+
+    it('should return an HttpResponseOK whose content is the swagger-ui.css asset.', async () => {
+      const controller = new ConcreteClass();
+      const response = await controller.swaggerUi();
+
+      if (!isHttpResponseOK(response)) {
+        throw new Error('SwaggerController.swaggerUi should return an HttpResponseOK instance.');
+      }
+
+      strictEqual(response.getHeader('Content-Type'), 'text/css');
+      strictEqual(response.stream, true);
+
+      const content = await streamToBuffer(response.body);
+      const expected = readFileSync('./node_modules/swagger-ui-dist/swagger-ui.css');
+      ok(expected.equals(content));
+    });
+
+  });
+
+  describe('has a "swaggerUiBundle" method that', () => {
+
+    it('should handle requests at GET /.', () => {
+      strictEqual(getHttpMethod(ConcreteClass, 'swaggerUiBundle'), 'GET');
+      strictEqual(getPath(ConcreteClass, 'swaggerUiBundle'), '/swagger-ui-bundle.js');
+    });
+
+    it('should return an HttpResponseOK whose content is the swagger-ui-bundle.js asset.', async () => {
+      const controller = new ConcreteClass();
+      const response = await controller.swaggerUiBundle();
+
+      if (!isHttpResponseOK(response)) {
+        throw new Error('SwaggerController.swaggerUiBundle should return an HttpResponseOK instance.');
+      }
+
+      strictEqual(response.getHeader('Content-Type'), 'application/javascript');
+      strictEqual(response.stream, true);
+
+      const content = await streamToBuffer(response.body);
+      const expected = readFileSync('./node_modules/swagger-ui-dist/swagger-ui-bundle.js');
+      ok(expected.equals(content));
+    });
+
+  });
+
+  describe('has a "swaggerUiStandalonePreset" method that', () => {
+
+    it('should handle requests at GET /.', () => {
+      strictEqual(getHttpMethod(ConcreteClass, 'swaggerUiStandalonePreset'), 'GET');
+      strictEqual(getPath(ConcreteClass, 'swaggerUiStandalonePreset'), '/swagger-ui-standalone-preset.js');
+    });
+
+    it('should return an HttpResponseOK whose content is the swagger-ui-standalone-preset.js asset.', async () => {
+      const controller = new ConcreteClass();
+      const response = await controller.swaggerUiStandalonePreset();
+
+      if (!isHttpResponseOK(response)) {
+        throw new Error('SwaggerController.swaggerUiStandalonePreset should return an HttpResponseOK instance.');
+      }
+
+      strictEqual(response.getHeader('Content-Type'), 'application/javascript');
+      strictEqual(response.stream, true);
+
+      const content = await streamToBuffer(response.body);
+      const expected = readFileSync('./node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js');
+      ok(expected.equals(content));
+    });
+
+  });
+
+});
--- packages/swagger/src/swagger-controller.ts
+++ packages/swagger/src/swagger-controller.ts
@@ -1,18 +1,98 @@
-import { Class, Context, Get } from '@foal/core';
+// std
+import { readFile } from 'fs';
+import { join } from 'path';
+import { promisify } from 'util';
 
+// 3p
+import { Class, Context, createHttpResponseFile, Get, HttpResponseOK, Options } from '@foal/core';
+import { getAbsoluteFSPath } from 'swagger-ui-dist';
+
+function isUrlOption(option): option is { url: string } {
+  return option.hasOwnProperty('url');
+}
+
+/**
+ *
+ *
+ * @export
+ * @abstract
+ * @class SwaggerController
+ */
 export abstract class SwaggerController {
-  options: { url: string } |
+  abstract options: { url: string } |
            { controllerClass: Class } |
            (
              { name: string, url: string, primary?: boolean } |
              { name: string, controllerClass: Class, primary?: boolean }
            )[];
 
+  /* Spec file(s) */
+
   @Get('swagger.json')
   getOpenApiDefinition(ctx: Context) {
     // Use a query /swagger.json?name=v1
   }
 
-  // et un get pour chaque fichier
+  /* UI */
+
+  @Get('/')
+  async index() {
+    const template = await promisify(readFile)(join(__dirname, 'index.tpl.html'), 'utf8');
+    let body = '';
+
+    if (!Array.isArray(this.options)) {
+      const url = isUrlOption(this.options) ? this.options.url : 'swagger.json';
+      body = template
+        .replace('{{ urls }}', `url: "${url}"`)
+        .replace('{{ urlArray }}', '');
+    } else {
+      let primaryName: string = '';
+      const options = this.options
+        .map(option => {
+          if (option.primary) {
+            primaryName = option.name;
+          }
+          return {
+            name: option.name,
+            url: isUrlOption(option) ? option.url : `swagger.json?name=${option.name}`
+          };
+        });
+      let urlArray = `\n      const urls = ${JSON.stringify(options)};`;
+      if (primaryName) {
+        urlArray = urlArray.concat(`\n      urls.primaryName = "${primaryName}";`);
+      }
+      body = template
+        .replace('{{ urls }}', 'urls: urls')
+        .replace('{{ urlArray }}', urlArray);
+    }
+
+    const response = new HttpResponseOK(body);
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return response;
+  }
+
+  @Get('/swagger-ui.css')
+  swaggerUi() {
+    return createHttpResponseFile({
+      directory: getAbsoluteFSPath(),
+      file: 'swagger-ui.css'
+    });
+  }
+
+  @Get('/swagger-ui-bundle.js')
+  swaggerUiBundle() {
+    return createHttpResponseFile({
+      directory: getAbsoluteFSPath(),
+      file: 'swagger-ui-bundle.js'
+    });
+  }
+
+  @Get('/swagger-ui-standalone-preset.js')
+  swaggerUiStandalonePreset() {
+    return createHttpResponseFile({
+      directory: getAbsoluteFSPath(),
+      file: 'swagger-ui-standalone-preset.js'
+    });
+  }
 
 }
