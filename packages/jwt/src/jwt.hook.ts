c5436261013aa8d614f68852aad4f709c81c9f50
Loïc Poullain <loic.poullain@centraliens.net>
2019-05-14 14:22:42 +0200

    typo

--- packages/jwt/src/jwt.hook.ts
+++ packages/jwt/src/jwt.hook.ts
@@ -33,7 +33,7 @@ class InvalidRequestResponse extends HttpResponseBadRequest {
  */
 export interface JWTOptions {
   user?: (id: string|number) => Promise<any|undefined>;
-  secretOrPublicKey?: (header: object, payload: object) => Promise<string>;
+  secretOrPublicKey?: (header: any, payload: any) => Promise<string>;
   blackList?: (token: string) => boolean|Promise<boolean>;
   cookie?: boolean;
 }
