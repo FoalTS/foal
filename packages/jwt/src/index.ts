b8c4e3bbcd150f9c1bb560612c35509c8f329aa6
Loïc Poullain <loic.poullain@centraliens.net>
2019-05-27 17:28:44 +0200

    [@foal/jwt] Add InvalidTokenError

--- packages/jwt/src/index.ts
+++ packages/jwt/src/index.ts
@@ -1,3 +1,4 @@
+export { InvalidTokenError, isInvalidTokenError } from './invalid-token.error';
 export { JWTOptions } from './jwt.hook';
 export { JWTOptional } from './jwt-optional.hook';
 export { JWTRequired } from './jwt-required.hook';
