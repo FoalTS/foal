a395af534177309061bdcbe63d3c7703f432d427
Loïc Poullain <loic.poullain@centraliens.net>
2019-05-28 20:02:15 +0200

    [@foal/jwks-rsa] Add Auth0 acceptance test

--- packages/acceptance-tests/src/jwks.spec.ts
+++ packages/acceptance-tests/src/jwks.spec.ts
@@ -1,11 +1,17 @@
-import { createApp, Get, HttpResponseOK } from '@foal/core';
-import { getRSAPublicKeyFromJWKS } from '@foal/jwks-rsa';
-import { JWTRequired } from '@foal/jwt';
+// std
 import { deepStrictEqual } from 'assert';
 import { readFileSync } from 'fs';
-import { sign } from 'jsonwebtoken';
 import { join } from 'path';
+
+// 3p
+import { sign } from 'jsonwebtoken';
 import * as superagent from 'superagent';
+import * as request from 'supertest';
+
+// FoalTS
+import { Config, createApp, Get, HttpResponseOK } from '@foal/core';
+import { getRSAPublicKeyFromJWKS } from '@foal/jwks-rsa';
+import { JWTRequired } from '@foal/jwt';
 
 describe('FoalTS should support authentification with a JWKS retreived', () => {
 
@@ -28,6 +34,7 @@ describe('FoalTS should support authentification with a JWKS retreived', () => {
           e: 'AQAB',
           kid: 'aaa',
           kty: 'RSA',
+          alg: 'RS256',
           n: 'vzoCEC2rpSpJQaWZbUmlsDNwp83Jr4fi6KmBWIwnj1MZ6CUQ7rBasuLI8AcfX5_10scSfQNCsTLV2t'
             + 'MKQaHuvyrVfwY0dINk-nkqB74QcT2oCCH9XduJjDuwWA4xLqAKuF96FsIes52opEM50W7_W7DZCKX'
             + 'kC8fFPFj6QF5ZzApDw2Qsu3yMRmr7_W9uWeaTwfPx24YdY7Ah-fdLy3KN40vXv9c4xiSafVvnx9Bw'
@@ -74,11 +81,47 @@ describe('FoalTS should support authentification with a JWKS retreived', () => {
     }
   });
 
-  describe('from Auth0.', () => {
+  it('from Auth0.', () => {
+    const domain = Config.get('auth0.domain');
+    const audience = Config.get('auth0.audience');
+    const token = Config.get('auth0.token');
+    class AppController {
 
+      @Get('/api/users/me')
+      @JWTRequired({
+        secretOrPublicKey: getRSAPublicKeyFromJWKS({
+          cache: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `https://${domain}/.well-known/jwks.json`,
+          rateLimit: true,
+        })
+      }, {
+        algorithms: [ 'RS256' ],
+        audience,
+        issuer: `https://${domain}/`,
+      })
+      getUser() {
+        return new HttpResponseOK({
+          name: 'Alix'
+        });
+      }
+
+    }
+
+    const app = createApp(AppController);
+
+    return request(app)
+      .get('/api/users/me')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200)
+      .then(response => {
+        deepStrictEqual(response.body, {
+          name: 'Alix'
+        });
+      });
   });
 
-  describe('from AWS Cognito.', () => {
+  xit('from AWS Cognito.', () => {
 
   });
 
