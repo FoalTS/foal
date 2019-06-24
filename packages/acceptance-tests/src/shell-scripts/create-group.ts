ec507db078a5615b80c85dce775925a26083c0cf
Loïc Poullain <loic.poullain@centraliens.net>
2019-06-24 09:53:07 +0200

    [Acceptance tests] Add create-{group|perm}

---
+++ packages/acceptance-tests/src/shell-scripts/create-group.ts
@@ -0,0 +1,46 @@
+// 3p
+import { Group, Permission } from '@foal/typeorm';
+import { createConnection, getConnection, getManager, getRepository } from 'typeorm';
+
+export const schema = {
+  additionalProperties: false,
+  properties: {
+    codeName: { type: 'string', maxLength: 100 },
+    name: { type: 'string', maxLength: 80 },
+    permissions: { type: 'array', items: { type: 'string' }, uniqueItems: true, default: [] }
+  },
+  required: [ 'name', 'codeName' ],
+  type: 'object',
+};
+
+export async function main(args) {
+  const group = new Group();
+  group.permissions = [];
+  group.codeName = args.codeName;
+  group.name = args.name;
+
+  await createConnection({
+    database: './e2e_db.sqlite',
+    entities: [ Permission, Group ],
+    type: 'sqlite',
+  });
+
+  for (const codeName of args.permissions as string[]) {
+    const permission = await getRepository(Permission).findOne({ codeName });
+    if (!permission) {
+      console.log(`No permission with the code name "${codeName}" was found.`);
+      return;
+    }
+    group.permissions.push(permission);
+  }
+
+  try {
+    console.log(
+      await getManager().save(group)
+    );
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await getConnection().close();
+  }
+}
---
+++ packages/acceptance-tests/src/shell-scripts/create-perm.ts
@@ -0,0 +1,35 @@
+// 3p
+import { Permission } from '@foal/typeorm';
+import { createConnection, getConnection, getManager } from 'typeorm';
+
+export const schema = {
+  additionalProperties: false,
+  properties: {
+    codeName: { type: 'string', maxLength: 100 },
+    name: { type: 'string' },
+  },
+  required: [ 'name', 'codeName' ],
+  type: 'object',
+};
+
+export async function main(args) {
+  const permission = new Permission();
+  permission.codeName = args.codeName;
+  permission.name = args.name;
+
+  await createConnection({
+    database: './e2e_db.sqlite',
+    entities: [ Permission ],
+    type: 'sqlite',
+  });
+
+  try {
+    console.log(
+      await getManager().save(permission)
+    );
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    await getConnection().close();
+  }
+}
