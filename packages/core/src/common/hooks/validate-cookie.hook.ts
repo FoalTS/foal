3e04a1a0398e5af0271ddb82b05933e7b690ea44
Loïc Poullain <loic.poullain@centraliens.net>
2019-06-27 12:06:40 +0200

    ValidateCookies -> ValidateCookie

--- packages/core/src/common/hooks/validate-cookie.hook.ts
+++ packages/core/src/common/hooks/validate-cookie.hook.ts
@@ -8,24 +8,29 @@ import { getAjvInstance } from '../utils';
 import { extractProperties } from './extract-properties.util';
 
 /**
- * Hook factory validating the cookies of the request against a AJV schema.
+ * Hook - Validate a specific cookie against an AJV schema.
  *
  * @export
  * @param {object} schema - Schema used to validate the cookies request.
  * @param {{ openapi?: boolean }} [options={}] - Options to add openapi metadata
  * @returns {HookDecorator} - The hook.
  */
-export function ValidateCookies(schema: object, options: { openapi?: boolean } = {}): HookDecorator {
-  if ((schema as any).type !== 'object') {
-    throw new Error('ValidateCookies only accepts a schema of type "object".');
-  }
-
+export function ValidateCookie(
+  name: string, schema: object, options: { openapi?: boolean, required?: boolean } = {}
+): HookDecorator {
   const ajv = getAjvInstance();
-  const isValid = ajv.compile(schema);
+  const required = options.required !== false;
 
   function validate(ctx: Context) {
-    if (!isValid(ctx.request.cookies)) {
-      return new HttpResponseBadRequest(isValid.errors as Ajv.ErrorObject[]);
+    const cookiesSchema = {
+      properties: {
+        [name]: schema
+      },
+      required: required ? [ name ] : [],
+      type: 'object',
+    };
+    if (!ajv.validate(cookiesSchema, ctx.request.cookies)) {
+      return new HttpResponseBadRequest({ cookies: ajv.errors });
     }
   }
 
@@ -36,19 +41,12 @@ export function ValidateCookies(schema: object, options: { openapi?: boolean } =
       return;
     }
 
-    for (const property of extractProperties(schema)) {
-      const apiCookieParameter: IApiCookieParameter = {
-        in: 'cookie',
-        name: property.name,
-        schema: property.schema
-      };
-      if (property.required) {
-        apiCookieParameter.required = true;
-      }
-
-      ApiParameter(apiCookieParameter)(target, propertyKey);
+    const apiCookieParameter: IApiCookieParameter = { in: 'cookie', name, schema };
+    if (required) {
+      apiCookieParameter.required = required;
     }
 
+    ApiParameter(apiCookieParameter)(target, propertyKey);
     ApiResponse(400, { description: 'Bad request.' })(target, propertyKey);
   };
 }
