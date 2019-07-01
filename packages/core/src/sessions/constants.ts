5db7ebbf720a1e9be1ff1a08a3ce24fe4e3dce13
Loïc Poullain <loic.poullain@centraliens.net>
2019-07-01 19:41:25 +0200

    [Sessions] Change cookie name

--- packages/core/src/sessions/constants.ts
+++ packages/core/src/sessions/constants.ts
@@ -4,7 +4,7 @@
 
 export const SESSION_DEFAULT_COOKIE_PATH: string = '/';
 export const SESSION_DEFAULT_COOKIE_HTTP_ONLY: boolean = true;
-export const SESSION_DEFAULT_COOKIE_NAME: string = 'auth';
+export const SESSION_DEFAULT_COOKIE_NAME: string = 'sessionID';
 
 // Expiration timeouts in seconds
 export const SESSION_DEFAULT_INACTIVITY_TIMEOUT = 15 * 60; // 15 minutes
