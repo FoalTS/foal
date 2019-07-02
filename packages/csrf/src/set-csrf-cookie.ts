f67f39445b5e3be8ae3771afb3ee03b65dfd31fe
Loïc Poullain <loic.poullain@centraliens.net>
2019-07-02 12:31:22 +0200

    [CSRF] Add setCsrfCookie

--- packages/csrf/src/set-csrf-cookie.ts
+++ packages/csrf/src/set-csrf-cookie.ts
@@ -1,5 +1,15 @@
-import { HttpResponse, Session } from '@foal/core';
+import { Config, CookieOptions, HttpResponse } from '@foal/core';
+import { CSRF_DEFAULT_COOKIE_NAME, CSRF_DEFAULT_COOKIE_PATH } from './constants';
 
-export function setCsrfCookie(response: HttpResponse, session: Session): void {
-
+export function setCsrfCookie(response: HttpResponse, csrfToken: string): void {
+  const cookieName = Config.get('settings.csrf.cookie.name', CSRF_DEFAULT_COOKIE_NAME);
+  const options: CookieOptions = {
+    domain: Config.get('settings.csrf.cookie.domain'),
+    httpOnly: false,
+    maxAge: Config.get('settings.csrf.cookie.maxAge'),
+    path: Config.get('settings.csrf.cookie.path', CSRF_DEFAULT_COOKIE_PATH),
+    sameSite: Config.get('settings.csrf.cookie.sameSite'),
+    secure: Config.get('settings.csrf.cookie.secure')
+  };
+  response.setCookie(cookieName, csrfToken, options);
 }
