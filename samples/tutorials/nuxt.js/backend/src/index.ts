c74ec13ddda43b407783dc3d22944955675b0105
Loïc Poullain <loic.poullain@centraliens.net>
2019-09-09 11:52:16 +0200

    [Samples] [Nuxt] Add frontend

--- samples/tutorials/nuxt.js/backend/src/index.ts
+++ samples/tutorials/nuxt.js/backend/src/index.ts
@@ -5,15 +5,35 @@ import * as http from 'http';
 
 // 3p
 import { Config, createApp } from '@foal/core';
+import { Builder, Nuxt } from 'nuxt';
 import { createConnection } from 'typeorm';
 
 // App
 import { AppController } from './app/app.controller';
 
+// Import and Set Nuxt.js options
+const config = require('../../frontend/nuxt.config.js');
+config.dev = Config.get('settings.debug', true);
+
 async function main() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config);
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  } else {
+    await nuxt.ready();
+  }
+
   await createConnection();
 
-  const app = createApp(AppController);
+  const app = createApp(AppController, {
+    postMiddlewares: [
+      nuxt.render
+    ]
+  });
 
   const httpServer = http.createServer(app);
   const port = Config.get('port', 3001);
