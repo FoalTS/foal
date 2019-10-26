044bfbdf7760eeca494792bf239637dff00c175b
fer22f <fer22f@gmail.com>
2019-10-26 13:29:32 -0300

    [Docs] Fix tutorials for new ESLint config

--- samples/tutorials/01-simple-todo-list/src/migrations/1562858090923-add-todo-entity.ts
+++ samples/tutorials/01-simple-todo-list/src/migrations/1562858090923-add-todo-entity.ts
@@ -1,15 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
-export class addTodoEntity1562858090923 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`);
+export class AddTodoEntity1562858090923 implements MigrationInterface {
+    async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)');
+        await queryRunner.query('CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)');
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`DROP TABLE "todo"`);
+    async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TABLE "user"');
+        await queryRunner.query('DROP TABLE "todo"');
     }
-
 }
--- samples/tutorials/02-multi-user-todo-list-mpa/src/migrations/1562860801825-user-and-todo.ts
+++ samples/tutorials/02-multi-user-todo-list-mpa/src/migrations/1562860801825-user-and-todo.ts
@@ -1,35 +1,33 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
-export class userAndTodo1562860801825 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "temporary_user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, CONSTRAINT "UQ_ed766a9782779b8390a2a81f444" UNIQUE ("email"))`);
-        await queryRunner.query(`INSERT INTO "temporary_user"("id") SELECT "id" FROM "user"`);
-        await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`ALTER TABLE "temporary_user" RENAME TO "user"`);
-        await queryRunner.query(`CREATE TABLE "temporary_todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "temporary_todo"("id", "text") SELECT "id", "text" FROM "todo"`);
-        await queryRunner.query(`DROP TABLE "todo"`);
-        await queryRunner.query(`ALTER TABLE "temporary_todo" RENAME TO "todo"`);
-        await queryRunner.query(`CREATE TABLE "temporary_todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL, CONSTRAINT "FK_05552e862619dc4ad7ec8fc9cb8" FOREIGN KEY ("ownerId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`);
-        await queryRunner.query(`INSERT INTO "temporary_todo"("id", "text", "ownerId") SELECT "id", "text", "ownerId" FROM "todo"`);
-        await queryRunner.query(`DROP TABLE "todo"`);
-        await queryRunner.query(`ALTER TABLE "temporary_todo" RENAME TO "todo"`);
+export class UserAndTodo1562860801825 implements MigrationInterface {
+    async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE TABLE "temporary_user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, CONSTRAINT "UQ_ed766a9782779b8390a2a81f444" UNIQUE ("email"))');
+        await queryRunner.query('INSERT INTO "temporary_user"("id") SELECT "id" FROM "user"');
+        await queryRunner.query('DROP TABLE "user"');
+        await queryRunner.query('ALTER TABLE "temporary_user" RENAME TO "user"');
+        await queryRunner.query('CREATE TABLE "temporary_todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL)');
+        await queryRunner.query('INSERT INTO "temporary_todo"("id", "text") SELECT "id", "text" FROM "todo"');
+        await queryRunner.query('DROP TABLE "todo"');
+        await queryRunner.query('ALTER TABLE "temporary_todo" RENAME TO "todo"');
+        await queryRunner.query('CREATE TABLE "temporary_todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL, CONSTRAINT "FK_05552e862619dc4ad7ec8fc9cb8" FOREIGN KEY ("ownerId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)');
+        await queryRunner.query('INSERT INTO "temporary_todo"("id", "text", "ownerId") SELECT "id", "text", "ownerId" FROM "todo"');
+        await queryRunner.query('DROP TABLE "todo"');
+        await queryRunner.query('ALTER TABLE "temporary_todo" RENAME TO "todo"');
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "todo" RENAME TO "temporary_todo"`);
-        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "todo"("id", "text", "ownerId") SELECT "id", "text", "ownerId" FROM "temporary_todo"`);
-        await queryRunner.query(`DROP TABLE "temporary_todo"`);
-        await queryRunner.query(`ALTER TABLE "todo" RENAME TO "temporary_todo"`);
-        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "todo"("id", "text") SELECT "id", "text" FROM "temporary_todo"`);
-        await queryRunner.query(`DROP TABLE "temporary_todo"`);
-        await queryRunner.query(`ALTER TABLE "user" RENAME TO "temporary_user"`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "user"("id") SELECT "id" FROM "temporary_user"`);
-        await queryRunner.query(`DROP TABLE "temporary_user"`);
+    async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('ALTER TABLE "todo" RENAME TO "temporary_todo"');
+        await queryRunner.query('CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "ownerId" integer NOT NULL)');
+        await queryRunner.query('INSERT INTO "todo"("id", "text", "ownerId") SELECT "id", "text", "ownerId" FROM "temporary_todo"');
+        await queryRunner.query('DROP TABLE "temporary_todo"');
+        await queryRunner.query('ALTER TABLE "todo" RENAME TO "temporary_todo"');
+        await queryRunner.query('CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)');
+        await queryRunner.query('INSERT INTO "todo"("id", "text") SELECT "id", "text" FROM "temporary_todo"');
+        await queryRunner.query('DROP TABLE "temporary_todo"');
+        await queryRunner.query('ALTER TABLE "user" RENAME TO "temporary_user"');
+        await queryRunner.query('CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)');
+        await queryRunner.query('INSERT INTO "user"("id") SELECT "id" FROM "temporary_user"');
+        await queryRunner.query('DROP TABLE "temporary_user"');
     }
-
 }
