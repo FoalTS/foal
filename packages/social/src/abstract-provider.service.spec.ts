0d666875fcf5c92179033d7397f7e9cf580f97d6
Loïc Poullain <loic.poullain@centraliens.net>
2019-11-06 15:52:26 +0100

    [Social Auth] Implement the "redirect" method

--- packages/social/src/abstract-provider.service.spec.ts
+++ packages/social/src/abstract-provider.service.spec.ts
@@ -1,6 +1,158 @@
+// std
+import { notStrictEqual, ok, strictEqual } from 'assert';
+import { URLSearchParams } from 'url';
+
+// 3p
+import { ConfigMock, createService, isHttpResponseRedirect } from '@foal/core';
+
+// FoalTS
+import { AbstractProvider, SocialTokens } from './abstract-provider.service';
+
+const STATE_COOKIE_NAME = 'oauth2-state';
 
 describe('AbstractProvider', () => {
-  it('has a "redirect" method that', () => {
+
+  class ConcreteProvider extends AbstractProvider {
+    protected configPaths = {
+      clientId: 'settings.social.example.clientId',
+      clientSecret: 'settings.social.example.clientSecret',
+      redirectUri: 'settings.social.example.redirectUri'
+    };
+    protected authEndpoint = 'https://example2.com/auth';
+    protected tokenEndpoint = 'https://example2.com/token';
+    getUserFromTokens(tokens: SocialTokens) {
+      throw new Error('Method not implemented.');
+    }
+  }
+
+  let provider: ConcreteProvider;
+  let configInstance: ConfigMock;
+
+  before(() => {
+    configInstance = new ConfigMock();
+    configInstance.set('settings.social.example.clientId', 'xxx');
+    configInstance.set('settings.social.example.clientSecret', 'yyy');
+    configInstance.set('settings.social.example.redirectUri', 'https://example.com/callback');
+
+    provider = createService(ConcreteProvider, { configInstance });
+  });
+
+  describe('has a "redirect" method that', () => {
+
+    it('should return an HttpResponseRedirect object.', async () => {
+      const result = await provider.redirect();
+      strictEqual(isHttpResponseRedirect(result), true);
+    });
+
+    describe('should return an HttpResponseRedirect object', () => {
+
+      it('with a redirect path which contains a client ID, a response type, a redirect URI.', async () => {
+        const response = await provider.redirect();
+        ok(response.path.startsWith(
+          'https://example2.com/auth?response_type=code&client_id=xxx&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback'
+        ));
+      });
+
+      it('with a redirect path which does not contain a scope if none was provided.', async () => {
+        const response = await provider.redirect();
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('scope'), null);
+      });
+
+      it('with a redirect path which contains the scopes if any are provided by the class.', async () => {
+        class ConcreteProvider2 extends ConcreteProvider {
+          defaultScopes = [ 'scope1', 'scope2' ];
+        }
+        provider = createService(ConcreteProvider2, { configInstance });
+
+        const response = await provider.redirect();
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('scope'), 'scope1 scope2');
+      });
+
+      it('with a redirect path which contains the scopes if any are provided by the class'
+          + ' (custom separator).', async () => {
+        class ConcreteProvider2 extends ConcreteProvider {
+          defaultScopes = [ 'scope1', 'scope2' ];
+          scopeSeparator = ',';
+        }
+        provider = createService(ConcreteProvider2, { configInstance });
+
+        const response = await provider.redirect();
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('scope'), 'scope1,scope2');
+      });
+
+      it('with a redirect path which contains the scopes if any are provided to the method.', async () => {
+        class ConcreteProvider2 extends ConcreteProvider {
+          // This checks that the default scopes will be override.
+          defaultScopes = [ 'scope1', 'scope2' ];
+        }
+        provider = createService(ConcreteProvider2, { configInstance });
+
+        const response = await provider.redirect({
+          scopes: [ 'scope3', 'scope4' ]
+        });
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('scope'), 'scope3 scope4');
+      });
+
+      it('with a generated state to protect against CSRF attacks.', async () => {
+        const response = await provider.redirect();
+        const stateCookieValue = response.getCookie(STATE_COOKIE_NAME).value;
+        if (typeof stateCookieValue !== 'string') {
+          throw new Error('Cookie not found.');
+        }
+
+        const searchParams = new URLSearchParams(response.path);
+        const stateParamValue = searchParams.get('state');
+        if (typeof stateParamValue !== 'string') {
+          throw new Error('State parameter not found.');
+        }
+
+        strictEqual(stateParamValue, stateCookieValue);
+        notStrictEqual(stateCookieValue.length, 0);
+      });
+
+      it('with a redirect path which contains extra parameters if any are provided by the class.', async () => {
+        class ConcreteProvider2 extends ConcreteProvider {
+          baseAuthEndpointParams = {
+            foo: 'bar'
+          };
+        }
+        provider = createService(ConcreteProvider2, { configInstance });
+
+        const response = await provider.redirect();
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('foo'), 'bar');
+      });
+
+      it('with a redirect path which contains extra parameters if any are provided to the method.', async () => {
+        class ConcreteProvider2 extends ConcreteProvider {
+          baseAuthEndpointParams = {
+            // This checks that the base params will be extended.
+            foo: 'bar',
+            foobar: 'barfoo'
+          };
+        }
+        provider = createService(ConcreteProvider2, { configInstance });
+
+        const response = await provider.redirect({
+          params: { foo: 'bar2' }
+        });
+        const searchParams = new URLSearchParams(response.path);
+
+        strictEqual(searchParams.get('foo'), 'bar2');
+        strictEqual(searchParams.get('foobar'), 'barfoo');
+      });
+
+    });
 
   });
+
 });
--- packages/social/src/abstract-provider.service.ts
+++ packages/social/src/abstract-provider.service.ts
@@ -4,7 +4,7 @@ import { URL } from 'url';
 import { promisify } from 'util';
 
 // 3p
-import { Config, Context, HttpResponseRedirect } from '@foal/core';
+import { Config, Context, dependency, HttpResponseRedirect } from '@foal/core';
 
 export interface SocialTokens {
   accessToken: string;
@@ -20,6 +20,9 @@ export interface SocialUser {
 const STATE_COOKIE_NAME = 'oauth2-state';
 
 export abstract class AbstractProvider {
+  @dependency
+  configInstance: Config;
+
   protected readonly abstract configPaths: {
     clientId: string;
     clientSecret: string;
@@ -28,51 +31,51 @@ export abstract class AbstractProvider {
   protected readonly abstract authEndpoint: string;
   protected readonly abstract tokenEndpoint: string;
 
-  protected readonly baseAuthEndpointParams: object;
-  protected readonly baseTokenEndpointParams: object;
+  protected readonly baseAuthEndpointParams: object = {};
+  protected readonly baseTokenEndpointParams: object = {};
 
-  protected readonly defaultScopes = '';
+  protected readonly defaultScopes: string[] = [];
 
-  // protected scopeSeparator = ' ';
+  protected readonly scopeSeparator: string = ' ';
 
   private get config() {
     return {
-      clientId: Config.get<string>(this.configPaths.clientId),
-      clientSecret: Config.get<string>(this.configPaths.clientSecret),
-      redirectUri: Config.get<string>(this.configPaths.redirectUri),
+      clientId: this.configInstance.get<string>(this.configPaths.clientId),
+      clientSecret: this.configInstance.get<string>(this.configPaths.clientSecret),
+      redirectUri: this.configInstance.get<string>(this.configPaths.redirectUri),
     };
   }
 
   abstract getUserFromTokens(tokens: SocialTokens): any;
 
-  async redirect({ scope, params }: { scope?: string[], params?: object } = {}): Promise<HttpResponseRedirect> {
-    // // Build the authorization URL.
-    // const url = new URL(this.authEndpoint);
-    // url.searchParams.set('reponse_type', 'code');
-    // url.searchParams.set('client_id', this.config.clientId);
-    // url.searchParams.set('redirect_uri', this.config.redirectUri);
+  async redirect({ scopes, params }: { scopes?: string[], params?: object } = {}): Promise<HttpResponseRedirect> {
+    // Build the authorization URL.
+    const url = new URL(this.authEndpoint);
+    url.searchParams.set('response_type', 'code');
+    url.searchParams.set('client_id', this.config.clientId);
+    url.searchParams.set('redirect_uri', this.config.redirectUri);
 
-    // // Add a scope if one is given. Or use the default ones.
-    // if (scope && scope.length > 0) {
-    //   url.searchParams.set('scope', scope.join(' '));
-    // }
+    // Add the scopes if any are provided.
+    const actualScopes = scopes || this.defaultScopes;
+    if (actualScopes.length > 0) {
+      url.searchParams.set('scope', actualScopes.join(this.scopeSeparator));
+    }
 
-    // // Generate a state to protect against CSRF attacks.
-    // const state = await this.getState();
-    // url.searchParams.set('state', state);
+    // Generate a state to protect against CSRF attacks.
+    const state = await this.getState();
+    url.searchParams.set('state', state);
 
-    // // Add extra parameters to the URL.
-    // const extraParams = Object.assign({}, this.baseAuthEndpointParams, params);
-    // // tslint:disable-next-line:forin
-    // for (const key in extraParams) {
-    //   url.searchParams.set(key, extraParams[key]);
-    // }
+    // Add extra parameters to the URL.
+    const extraParams = Object.assign({}, this.baseAuthEndpointParams, params);
+    // tslint:disable-next-line:forin
+    for (const key in extraParams) {
+      url.searchParams.set(key, extraParams[key]);
+    }
 
-    // // Return a redirection response with the state as cookie.
-    // return new HttpResponseRedirect(url.href)
-    //   .setCookie(STATE_COOKIE_NAME, state);
+    // Return a redirection response with the state as cookie.
+    return new HttpResponseRedirect(url.href)
+      .setCookie(STATE_COOKIE_NAME, state);
     // Which cookie parameters? HTTPS vs HTTP.
-    return new HttpResponseRedirect('');
   }
 
   async getTokens(ctx: Context): Promise<SocialTokens> {
@@ -84,6 +87,7 @@ export abstract class AbstractProvider {
     // In the controller, maybe have a util "errorsToHttpResponses" to use in the "catch".
     //
     // Make a request to this.tokenEndpoint with the proper parameters (clientId, clientSecret, code, redirectUri).
+    // Use baseTokenEndpointParams.
     // Return the access token and additional tokens.
     return {
       accessToken: 'x',
@@ -105,8 +109,7 @@ export abstract class AbstractProvider {
   }
 
   private async getState(): Promise<string> {
-    // const buff = await promisify(randomBytes)(32);
-    // return buff.toString('base64');
-    return '';
+    const buff = await promisify(randomBytes)(32);
+    return buff.toString('base64');
   }
 }
