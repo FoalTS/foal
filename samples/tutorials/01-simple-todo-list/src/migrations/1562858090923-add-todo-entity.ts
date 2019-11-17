0347d33d413e51cc24f137a823c3f20188ad71a0
fer22f <fer22f@gmail.com>
2019-11-16 23:28:02 -0300

    [Docs] Revert migration prettifying

--- samples/tutorials/01-simple-todo-list/src/migrations/1562858090923-add-todo-entity.ts
+++ samples/tutorials/01-simple-todo-list/src/migrations/1562858090923-add-todo-entity.ts
@@ -1,13 +1,15 @@
-import {MigrationInterface, QueryRunner} from 'typeorm';
+import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddTodoEntity1562858090923 implements MigrationInterface {
-    async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)');
-        await queryRunner.query('CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)');
+export class addTodoEntity1562858090923 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL)`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`);
     }
 
-    async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('DROP TABLE "user"');
-        await queryRunner.query('DROP TABLE "todo"');
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TABLE "todo"`);
     }
+
 }
