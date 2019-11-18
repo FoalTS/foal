a3006a725ef61afe165b489774106c8f7301ab24
fer22f <fer22f@gmail.com>
2019-11-18 09:33:50 -0300

    [CLI] Add tsconfigRootDir so linting in vim works

--- samples/tutorials/nuxt.js/backend/.eslintrc.js
+++ samples/tutorials/nuxt.js/backend/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module'
   },
   plugins: [
