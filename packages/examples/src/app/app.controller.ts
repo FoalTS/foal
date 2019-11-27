553b777c7e06523356e3894e88603d391d03bed8
Loïc Poullain <loic.poullain@centraliens.net>
2019-11-27 10:27:34 +0100

    [@foal/examples] Use Google & FB social auth

--- packages/examples/src/app/app.controller.ts
+++ packages/examples/src/app/app.controller.ts
@@ -1,12 +1,23 @@
-import { controller } from '@foal/core';
+import { Context, controller, Get, render, Session, TokenRequired } from '@foal/core';
+import { TypeORMStore } from '@foal/typeorm';
 
-import { ApiV2Controller, AuthController, OpenApiController, ViewController } from './controllers';
+import { AuthController } from './controllers';
 
 export class AppController {
   subControllers = [
     controller('', AuthController),
-    controller('', ViewController),
-    controller('/api', ApiV2Controller),
-    controller('/swagger', OpenApiController)
   ];
+
+  @Get('/')
+  @TokenRequired({ cookie: true, store: TypeORMStore, redirectTo: '/signin' })
+  index(ctx: Context<any, Session>) {
+    return render('./templates/index.html', {
+      profile: JSON.stringify(ctx.session.get('profile'))
+    });
+  }
+
+  @Get('/signin')
+  signin() {
+    return render('./templates/signin.html');
+  }
 }
--- packages/examples/src/index.ts
+++ packages/examples/src/index.ts
@@ -28,4 +28,5 @@ async function main() {
   });
 }
 
-main();
+main()
+  .catch(err => { console.error(err); process.exit(1); });
