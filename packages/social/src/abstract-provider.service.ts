c0fd9796018655209ddb2ce463c6cb9322b6d818
Loïc Poullain <loic.poullain@centraliens.net>
2019-12-04 18:37:24 +0100

    [Social Auth] Remove comment.

--- packages/social/src/abstract-provider.service.ts
+++ packages/social/src/abstract-provider.service.ts
@@ -65,7 +65,6 @@ export class AuthorizationError extends Error {
       + `- description: ${errorDescription}\n`
       + `- URI: ${errorUri}`
     );
-    // this.name = 'AuthorizationError';
   }
 }
 
