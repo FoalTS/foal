21b3b2ba65b3657d2730c20836b0325bdda0cbe2
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-08 09:09:39 +0200

    Add unit doc

--- packages/typeorm/src/utils/fetch-mongodb-user.util.ts
+++ packages/typeorm/src/utils/fetch-mongodb-user.util.ts
@@ -3,6 +3,21 @@ import { Class } from '@foal/core';
 // tslint:disable-next-line:no-unused-variable
 import { getMongoRepository, ObjectID } from 'typeorm';
 
+/**
+ * Create a function that finds the first MongoDB entity that matches some id.
+ *
+ * It returns undefined if no entity can be found.
+ *
+ * This function is usually used by:
+ * - TokenRequired (@foal/core)
+ * - TokenOptional (@foal/core)
+ * - JWTRequired (@foal/jwt)
+ * - JWTOptional (@foal/jwt)
+ *
+ * @export
+ * @param {(Class<{ id: ObjectID }|{ _id: ObjectID }>)} userEntityClass - The entity class.
+ * @returns {((id: number|string) => Promise<any>)} The returned function expecting an id.
+ */
 export function fetchMongoDBUser(
   userEntityClass: Class<{ id: ObjectID }|{ _id: ObjectID }>
 ): (id: number|string) => Promise<any> {
