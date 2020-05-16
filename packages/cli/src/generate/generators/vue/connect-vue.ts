bd88b4f991d9319751b2687d128b6b84fd89087b
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-16 11:42:14 +0200

    [CLI] Refactor create-vuewith FileSystem

--- packages/cli/src/generate/generators/vue/connect-vue.ts
+++ packages/cli/src/generate/generators/vue/connect-vue.ts
@@ -1,26 +1,28 @@
 import { join, relative } from 'path';
 
 import { red } from 'colors/safe';
-import { existsSync } from 'fs';
-import { Generator } from '../../utils';
+import { FileSystem } from '../../file-system';
 
 export function connectVue(path: string) {
-  if (!existsSync(path)) {
+  const fs = new FileSystem();
+
+  if (!fs.exists(path)) {
     if (process.env.NODE_ENV !== 'test') {
       console.log(red(`  The directory ${path} does not exist.`));
     }
     return;
   }
 
-  if (!existsSync(join(path, 'package.json'))) {
+  if (!fs.exists(join(path, 'package.json'))) {
     if (process.env.NODE_ENV !== 'test') {
       console.log(red(`  The directory ${path} is not a Vue project (missing package.json).`));
     }
     return;
   }
 
-  new Generator('vue', path)
-    .updateFile('package.json', content => {
+  fs
+    .cd(path)
+    .modify('package.json', content => {
       const pkg = JSON.parse(content);
       pkg.vue = pkg.vue || {};
 
