a313bafe6421b26c0cf51ec86365e490e81cc1c1
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-16 12:06:41 +0200

    [CLI] Refactor connect-react with FileSystem

--- packages/cli/src/generate/generators/react/connect-react.ts
+++ packages/cli/src/generate/generators/react/connect-react.ts
@@ -1,26 +1,28 @@
 import { join } from 'path';
 
 import { red } from 'colors/safe';
-import { existsSync } from 'fs';
-import { Generator } from '../../utils';
+import { FileSystem } from '../../file-system';
 
 export function connectReact(path: string) {
-  if (!existsSync(path)) {
+  const fs = new FileSystem();
+
+  if (!fs.exists(path)) {
     if (process.env.NODE_ENV !== 'test') {
       console.log(red(`  The directory ${path} does not exist.`));
     }
     return;
   }
 
-  if (!existsSync(join(path, 'package.json'))) {
+  if (!fs.exists(join(path, 'package.json'))) {
     if (process.env.NODE_ENV !== 'test') {
       console.log(red(`  The directory ${path} is not a React project (missing package.json).`));
     }
     return;
   }
 
-  new Generator('react', path)
-    .updateFile('package.json', content => {
+  fs
+    .cd(path)
+    .modify('package.json', content => {
       const pkg = JSON.parse(content);
       pkg.proxy = 'http://localhost:3001';
       return JSON.stringify(pkg, null, 2);
