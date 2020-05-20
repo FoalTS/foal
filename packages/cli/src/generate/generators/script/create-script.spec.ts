d17722f45a863c79feda2d8c57ec41489263f90e
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-20 17:53:06 +0200

    [CLI] Use cdProjectRootDir in createScript

--- packages/cli/src/generate/generators/script/create-script.spec.ts
+++ packages/cli/src/generate/generators/script/create-script.spec.ts
@@ -1,47 +1,25 @@
 // FoalTS
 import { FileSystem } from '../../file-system';
-import {
-  rmDirAndFilesIfExist,
-} from '../../utils';
 import { createScript } from './create-script';
-
-// TODO: Improve the tests. They currently cover partially `createScript`.
 
 describe('createScript', () => {
 
   const fs = new FileSystem();
-  // TODO: remove this line.
-  (fs as any).testDir = '';
 
-  afterEach(() => {
-    rmDirAndFilesIfExist('src/scripts');
-    // We cannot remove src/ since the generator code lives within. This is bad testing
-    // approach.
-  });
+  beforeEach(() => fs.setUp());
 
-  describe(`when the directory src/scripts/ exists`, () => {
+  afterEach(() => fs.tearDown());
 
-    beforeEach(() => {
-      fs
-        .ensureDir('src/scripts')
-        .cd('src/scripts');
-    });
+  it('should copy the empty script file in the proper directory.', () => {
+    fs
+      .copyFixture('script/package.json', 'package.json')
+      .ensureDir('src/scripts');
 
-    it('should copy the empty script file in the proper directory.', () => {
-      createScript({ name: 'test-fooBar' });
+    createScript({ name: 'test-fooBar' });
 
-      fs
-        .assertEqual('test-foo-bar.ts', 'script/test-foo-bar.ts');
-    });
-
-  });
-
-  describe(`when the directory src/scripts/ does not exist`, () => {
-
-    it('should not throw an error.', () => {
-      createScript({ name: 'test-fooBar' });
-    });
-
+    fs
+    .cd('src/scripts')
+      .assertEqual('test-foo-bar.ts', 'script/test-foo-bar.ts');
   });
 
 });
--- packages/cli/src/generate/generators/script/create-script.ts
+++ packages/cli/src/generate/generators/script/create-script.ts
@@ -3,36 +3,18 @@ import { existsSync } from 'fs';
 import { join, relative } from 'path';
 
 // 3p
-import { red } from 'colors/safe';
-
 // FoalTS
 import { FileSystem } from '../../file-system';
-import { findProjectPath, getNames } from '../../utils';
+import { getNames } from '../../utils';
 
 export function createScript({ name }: { name: string }) {
   const names = getNames(name);
 
-  const root = findProjectPath();
-
-  if (!root) {
-    console.log(red('\n  This directory is not a Foal project (missing package.json).\n'));
-    return;
-  }
-
-  const scriptPath = join(root, './src/scripts/');
-  if (!existsSync(scriptPath)) {
-    if (process.env.P1Z7kEbSUUPMxF8GqPwD8Gx_FOAL_CLI_TEST !== 'true') {
-      console.log(red(`\n  This directory is not a Foal project (${scriptPath} does not exist).\n`));
-    }
-    return;
-  }
-
   const fs = new FileSystem();
-  // TODO: remove this line.
-  (fs as any).testDir = '';
 
   fs
-    // Use `relative` to have pretty CREATE logs.
-    .cd(relative(process.cwd(), scriptPath))
+    // TODO: test this line
+    .cdProjectRootDir()
+    .cd('src/scripts')
     .copy('script/script.ts', `${names.kebabName}.ts`);
 }
