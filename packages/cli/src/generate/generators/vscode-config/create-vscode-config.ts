9d3df4954e5a595793362f78c694dd343077bcdc
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-21 09:05:41 +0200

    [CLI] create-vscodeconfig goes to root directory

--- packages/cli/src/generate/generators/vscode-config/create-vscode-config.ts
+++ packages/cli/src/generate/generators/vscode-config/create-vscode-config.ts
@@ -3,6 +3,8 @@ import { FileSystem } from '../../file-system';
 
 export function createVSCodeConfig() {
   new FileSystem()
+    // TODO: test this line
+    .cdProjectRootDir()
     .ensureDir('.vscode')
     .cd('.vscode')
     .copy('vscode-config/launch.json', 'launch.json')
