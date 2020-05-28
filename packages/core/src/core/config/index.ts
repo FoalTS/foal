085f478b74a5a13357f3d11ce61af8aa07759c3c
Loïc Poullain <loic.poullain@centraliens.net>
2020-05-28 16:21:49 +0200

    Remove ConfigMock

--- packages/core/src/core/config/index.ts
+++ packages/core/src/core/config/index.ts
@@ -1,4 +1,3 @@
-export { ConfigMock } from './config-mock';
 export { ConfigNotFoundError } from './config-not-found.error';
 export { ConfigTypeError } from './config-type.error';
 export { Config } from './config';
