77bf44033a033082a897d1207e2d88fde7cf8388
Loïc Poullain <loic.poullain@centraliens.net>
2020-08-27 17:12:47 +0200

    [Acceptance tests] Add utils

---
+++ packages/acceptance-tests/src/common/user.entity.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
