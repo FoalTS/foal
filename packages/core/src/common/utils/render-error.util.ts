52b1e6a5967226e84dbc54d476762015159d4cf2
Loïc Poullain <loic.poullain@centraliens.net>
2020-09-17 17:19:02 +0200

    Add error and ctx to renderError response

--- packages/core/src/common/utils/render-error.util.ts
+++ packages/core/src/common/utils/render-error.util.ts
@@ -7,30 +7,29 @@ import { promisify } from 'util';
 import { Config, Context, HttpResponseInternalServerError } from '../../core';
 import { renderToString } from './render.util';
 
-const page500 = '<html><head><title>INTERNAL SERVER ERROR</title></head><body>'
-                + '<h1>500 - INTERNAL SERVER ERROR</h1></body></html>';
-
 /**
- * Render the default HTML page when an error is thrown or rejected in the application.
+ * Renders the default HTML page when an error is thrown or rejected in the application.
  *
  * The page is different depending on if the configuration key `settings.debug` is
  * true or false.
  *
  * @export
  * @param {Error} error - The error thrown or rejected.
- * @param {Context} ctx - The Context. object.
+ * @param {Context} ctx - The Context object.
  * @returns {Promise<HttpResponseInternalServerError>} The HTTP response.
  */
 export async function renderError(error: Error, ctx: Context): Promise<HttpResponseInternalServerError> {
-  const template = await promisify(readFile)(join(__dirname, '500.debug.html'), 'utf8');
+  let body = '<html><head><title>INTERNAL SERVER ERROR</title></head><body>'
+  + '<h1>500 - INTERNAL SERVER ERROR</h1></body></html>';
 
-  if (!Config.get('settings.debug', 'boolean')) {
-    return new HttpResponseInternalServerError(page500);
+  if (Config.get('settings.debug', 'boolean')) {
+    const template = await promisify(readFile)(join(__dirname, '500.debug.html'), 'utf8');
+    body = renderToString(template, {
+      message: error.message,
+      name: error.name,
+      stack: error.stack
+    });
   }
 
-  return new HttpResponseInternalServerError(renderToString(template, {
-    message: error.message,
-    name: error.name,
-    stack: error.stack
-  }));
+  return new HttpResponseInternalServerError(body, { ctx, error });
 }
