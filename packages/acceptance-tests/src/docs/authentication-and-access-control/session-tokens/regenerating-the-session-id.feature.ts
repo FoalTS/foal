a2de77f44200f570ef13f8745bd42dc7576b9054
Loïc Poullain <loic.poullain@centraliens.net>
2021-03-12 15:32:05 +0100

    [Docs] Test "regenerate ID"

---
+++ packages/acceptance-tests/src/docs/authentication-and-access-control/session-tokens/regenerating-the-session-id.feature.ts
@@ -0,0 +1,82 @@
+// std
+import { notStrictEqual } from 'assert';
+
+// 3p
+import * as request from 'supertest';
+
+// FoalTS
+import {
+  Config,
+  Context,
+  createApp,
+  createSession,
+  dependency,
+  Get,
+  HttpResponseOK,
+  IAppController,
+  Session,
+  Store,
+  UseSessions
+} from '@foal/core';
+import { DatabaseSession } from '@foal/typeorm';
+import { closeTestConnection, createTestConnection, getTypeORMStorePath } from '../../../common';
+
+describe.only('Feature: Regenerating the session ID', () => {
+
+  beforeEach(() => {
+    Config.set('settings.session.store', getTypeORMStorePath());
+  });
+
+  afterEach(() => {
+    Config.remove('settings.session.store');
+    return closeTestConnection();
+  });
+
+  it('Example: Simple Example.', async () => {
+
+    @UseSessions()
+    class AppController implements IAppController {
+      @dependency
+      store: Store;
+
+      @Get('/new-session')
+      async createSessionAndReturnToken(ctx: Context) {
+        ctx.session = await createSession(this.store);
+        return new HttpResponseOK({ token: ctx.session.getToken() });
+      }
+
+      @Get('/regenerated-id')
+      async regenerateID(ctx: Context<any, Session>) {
+        /* ======================= DOCUMENTATION BEGIN ======================= */
+
+        await ctx.session.regenerateID();
+
+        /* ======================= DOCUMENTATION END ========================= */
+
+        return new HttpResponseOK({ token: ctx.session.getToken() });
+      }
+
+      async init() {
+        await createTestConnection([ DatabaseSession ]);
+      }
+    }
+
+    const app = await createApp(AppController);
+
+    let token = '';
+
+    await request(app)
+      .get('/new-session')
+      .expect(200)
+      .then(response => token = response.body.token);
+
+    await request(app)
+      .get('/regenerated-id')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .then(response => {
+        notStrictEqual(response.body.token, token);
+      });
+  });
+
+});
