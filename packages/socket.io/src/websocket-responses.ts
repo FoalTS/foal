0f75b894e57b1945fcc17494adda1445c9b81dcb
Loïc Poullain <loic.poullain@centraliens.net>
2021-04-06 17:35:31 +0200

    [WS] Make response payload optional

--- packages/socket.io/src/websocket-responses.ts
+++ packages/socket.io/src/websocket-responses.ts
@@ -1,7 +1,7 @@
 export class WebsocketResponse {
-  constructor(public payload: any) {}
+  constructor(public payload?: any) {}
 }
 
 export class WebsocketErrorResponse {
-  constructor(public payload: any) {}
+  constructor(public payload?: any) {}
 }
