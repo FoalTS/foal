ae801f329069daaeecd468faa10c6c958c89ce9f
Loïc Poullain <loic.poullain@centraliens.net>
2021-04-08 11:40:00 +0200

    [WS] Implement SocketIOController

--- packages/socket.io/src/socketio-controller.service.ts
+++ packages/socket.io/src/socketio-controller.service.ts
@@ -1,10 +1,75 @@
+// 3p
+import { Class, dependency, ServiceManager } from '@foal/core';
+import { Server, ServerOptions } from 'socket.io';
+import { Adapter } from 'socket.io-adapter';
+
 // FoalTS
+import { getWebsocketResponse } from './routes/get-websocket-response';
+import { makeWebsocketControllerRoutes } from './routes/make-websocket-controller-routes';
 import { ISocketIOController } from './routes/socketio-controller.interface';
+import { WebsocketContext } from './websocket-context';
+import { WebsocketErrorResponse, WebsocketResponse } from './websocket-responses';
 
-export class SocketIOController implements ISocketIOController {
+export abstract class SocketIOController implements ISocketIOController {
+  @dependency
+  services: ServiceManager;
 
-  attachHttpServer(server: any) {
+  adapter: typeof Adapter|undefined;
 
+  options: Partial<ServerOptions> = {};
+
+  private wsServer: Server|undefined;
+
+  async attachHttpServer(httpServer: any): Promise<void> {
+    this.wsServer = new Server(httpServer, this.options);
+    if (this.adapter) {
+      this.wsServer.adapter(this.adapter);
+    }
+    const routes = Array.from(makeWebsocketControllerRoutes(this.constructor as Class<SocketIOController>, this.services));
+
+    this.wsServer.use(async (socket, next) => {
+      const connectionCtx = new WebsocketContext('', undefined, socket);
+
+      try {
+        await this.onConnection(connectionCtx);
+      } catch (error) {
+        return next(error);
+      }
+
+      next();
+    })
+
+    this.wsServer.on('connection', socket => {
+      for (const route of routes) {
+        socket.on(route.eventName, async (payload, cb) => {
+          const ctx = new WebsocketContext(route.eventName, payload, socket);
+          const response = await getWebsocketResponse(route, ctx, this.services, this);
+          if (response instanceof WebsocketResponse) {
+            return cb({
+              status: 'ok',
+              data: response.payload,
+            });
+          }
+          if (response instanceof WebsocketErrorResponse) {
+            return cb({
+              status: 'error',
+              error: response.payload
+            });
+          }
+          return cb();
+        });
+      }
+    });
+
+    await this.services.boot();
+  }
+
+  onConnection(ctx: WebsocketContext): void|Promise<void> {}
+
+  close(): void {
+    if (this.wsServer) {
+      this.wsServer.close();
+    }
   }
 
 }
