4e755045f96219ff463cfd45038736208696443b
Loïc Poullain <loic.poullain@centraliens.net>
2021-06-29 11:34:35 +0200

    [Website] Fix newsletter page height on Chrome

--- docs/src/pages/newsletter.jsx
+++ docs/src/pages/newsletter.jsx
@@ -5,7 +5,7 @@ export default function Newsletter() {
   return (
     <Layout
       description='Suscribe to FoalTS newsletter'>
-        <iframe style={{ height: '100%', backgroundColor: '#1b7cf2' }} className="mj-w-res-iframe" frameBorder={0} scrolling="no" marginHeight="0" marginWidth="0" src="https://app.mailjet.com/widget/iframe/6owE/Hce" width="100%"></iframe>
+        <iframe style={{ height: '300px', backgroundColor: '#1b7cf2' }} className="mj-w-res-iframe" frameBorder={0} scrolling="no" marginHeight="0" marginWidth="0" src="https://app.mailjet.com/widget/iframe/6owE/Hce" width="100%"></iframe>
     </Layout>
   );
 }
