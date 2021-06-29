153155ad5f5be6390c9696683ea0e3d8da4572b3
Loïc Poullain <loic.poullain@centraliens.net>
2021-06-29 11:08:11 +0200

    [Website] Fix newsletter page width

--- docs/src/pages/newsletter.jsx
+++ docs/src/pages/newsletter.jsx
@@ -5,9 +5,7 @@ export default function Newsletter() {
   return (
     <Layout
       description='Suscribe to FoalTS newsletter'>
-        <div style={{ textAlign: 'center' }}>
-          <iframe className="mj-w-res-iframe" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://app.mailjet.com/widget/iframe/6owE/Hce" width="633" height="436"></iframe>
-        </div>
+        <iframe className="mj-w-res-iframe" frameBorder={0} scrolling="no" marginHeight="0" marginWidth="0" src="https://app.mailjet.com/widget/iframe/6owE/Hce" width="100%"></iframe>
     </Layout>
   );
 }
