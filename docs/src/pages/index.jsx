abf4c2facaaf5df4d5db7eecd22f9cfca0380d96
Loïc Poullain <loic.poullain@centraliens.net>
2021-07-01 19:24:17 +0200

    [Website] Add feature section

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -5,68 +5,28 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './index.module.scss';
 
-const features = [
-  {
-    title: 'Full featured',
-    description: (
-      <>
-        Achieve more by reusing ready-made components.
-        <br />
-        <br />
-        You don't need to rebuild everything from scratch
-        or waste time finding all the pieces you need on
-        npm and making them work together. The foundation
-        is already there.
-      </>
-    ),
-  },
-  {
-    title: 'Expressive code',
-    description: (
-      <>
-        Foal always strives to provide you with the simplest possible code.
-        <br />
-        <br />
-        Complexity and unnecessary abstractions are put aside. The components
-        are designed to be easy to use and understand, so you don't have to
-        spend more time reading the documentation than you do coding.
-      </>
-    ),
-  },
-  {
-    title: 'TypeScript Based',
-    description: (
-      <>
-        TypeScript brings you optional static type-checking along with the
-        latest ECMAScript features.
-        <br />
-        <br />
-        This allows you to detect most silly
-        errors during compilation and improve the quality of your code.
-        It also offers you autocompletion and a well documented API.
-      </>
-    ),
-  },
-];
-
-function Feature({ title, description }) {
+function FeatureCell(props) {
   return (
-    <div className={styles.col}>
-      <h3>{title}</h3>
-      <p>{description}</p>
+    <div className={styles.featureCell}>
+      <div className={styles.featureSymbol}>
+        ›
+      </div>
+      <div className={styles.featureContent}>
+        <h3>{props.title}</h3>
+        <p>{props.children}</p>
+      </div>
     </div>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
   return (
     <Layout  
       description="Elegant and complete Node.Js web framework based on TypeScript.">
         <header className={styles.masthead}>
           <div className={styles.content}>
-            <h1>The elegant and complete <span>NodeJS</span> framework</h1>
+            <h1>The elegant and complete <strong>NodeJS</strong> framework</h1>
             <h3>Simple, flexible and fully-featured.</h3>
             <div>
               <Link
@@ -82,20 +42,103 @@ function Home() {
             </div>
           </div>
         </header>
-        <section className={styles.main}>
-          <div>
+        <section>
+          <div className={styles.screenshotContainer}>
             <img src={useBaseUrl('img/home/screenshot.png')} alt="" />
           </div>
-          {features && features.length > 0 && (
-            <section className={styles.features}>
-              {features.map((props, idx) => (
-                <Feature key={idx} {...props} />
-              ))}
-            </section>
-          )}
-          <div>
+          <div className={`${styles.metrics} ${styles.section2} ${styles.smallSection}`}>
+            <div>
+              <div className={styles.number}>2,100<span className={styles.plus}>+</span></div>
+              <div className={styles.caption}>acceptance <br/> and unit tests</div>
+            </div>
+            <div>
+              <div className={styles.number}>11,000<span className={styles.plus}>+</span></div>
+              <div className={styles.caption}>lines of <br/>documentation</div>
+            </div>
+            <div>
+              <div className={styles.number}>4 years</div>
+              <div className={styles.caption}>of active <br/>development</div>
+            </div>
+          </div>
+          <div className={`${styles.section1} ${styles.feature}`}>
+            <div className={styles.col1}>
+              <h2>All-in-One Framework</h2>
+              <p>
+                The foundation is already there.
+                You don't have to rebuild everything from scratch or make 3rd-party packages work together.
+                Everything is included.
+                <br/>
+                <br/>
+                But if you wish, you can still import and use your favorite libraries. The framework is extensible.
+              </p>
+            </div>
+            <div className={styles.col2}>
+              <div className={styles.featureRow}>
+                <FeatureCell title="CLI">
+                  Build and run your app in development and in production. Generate files.
+                </FeatureCell>
+                <FeatureCell title="ORM">
+                  Take advantage of TypeORM and generate migrations from your models.
+                </FeatureCell>
+              </div>
+              <div className={styles.featureRow}>
+                <FeatureCell title="JWTs and Session Tokens">
+                  Implement stateful or stateless authentication with cookies or the Authorization header.
+                </FeatureCell>
+                <FeatureCell title="Unit and E2E tests">
+                  Write automated tests with a ready-made environment.
+                </FeatureCell>
+              </div>
+              <div className={styles.featureRow}>
+                <FeatureCell title="Swagger Generation">
+                  Generate an OpenAPI specification and a Swagger page directly from your code.
+                </FeatureCell>
+                <FeatureCell title="Roles and Permissions">
+                  Control routes access with static roles or dynamic permissions.
+                </FeatureCell>
+              </div>
+              <div className={styles.featureRow}>
+                <FeatureCell title="Shell Scripts">
+                  Create scripts to be run from the command line with argument validation.
+                </FeatureCell>
+                <FeatureCell title="File Upload and Storage">
+                  Validate uploaded files and save them in local or in the Cloud (AWS S3).
+                </FeatureCell>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.section2} ${styles.feature}`}>
+            <div className={styles.col2}>
+
+            </div>
+            <div className={styles.col1}>
+              <h2>TypeScript Support</h2>
+              <p>
+                Thanks to TypeScript, most careless errors are caught during compilation
+                and the overall quality of your code is improved. The language also offers
+                you autocompletion and a well-documented API.
+              </p>
+            </div>
+          </div>
+          <div className={`${styles.section1} ${styles.feature}`}>
+            <div className={styles.col1}>
+              <h2>Simple and Intuitive Architecture</h2>
+              <p>
+                Complexity and unnecessary abstractions are put aside. Foal's components
+                are designed to be easy to use and understand, so you don't have to
+                spend more time reading the documentation than you do coding.
+              </p>
+            </div>
+            <div className={styles.col2}>
+
+            </div>
+          </div>
+          <div className={`${styles.section2} ${styles.testimonials}`}>
+            <h2>What they say about Foal</h2>
+          </div>
+          <div className={`${styles.section1} ${styles.getStarted}`}>
             <Link
-              className={styles.btn}
+              className={`${styles.btn} ${styles.btnMedium} ${styles.btnPrimary}`}
               to={useBaseUrl("docs/tutorials/simple-todo-list/1-installation")}>
               Get started
             </Link>
