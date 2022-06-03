c18b5c95435858505bcb0c34f92d7605633adde2
Loïc Poullain <loic.poullain@centraliens.net>
2022-06-03 20:57:30 +0200

    [CLI] Use dataSource in E2E tests

--- packages/cli/src/generate/specs/app/src/e2e/index.ts
+++ packages/cli/src/generate/specs/app/src/e2e/index.ts
@@ -1,20 +1,25 @@
 // 3p
 import { createApp } from '@foal/core';
 import * as request from 'supertest';
-import { getConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
 
 // App
 import { AppController } from '../app/app.controller';
+import { createDataSource } from '../app/create-data-source';
 
 describe('The server', () => {
 
   let app;
+  let dataSource: DataSource;
 
   before(async () => {
+    dataSource = createDataSource();
+    await dataSource.initialize();
+
     app = await createApp(AppController);
   });
 
-  after(() => getConnection().close());
+  after(() => dataSource.destroy());
 
   it('should return a 200 status on GET / requests.', () => {
     return request(app)
