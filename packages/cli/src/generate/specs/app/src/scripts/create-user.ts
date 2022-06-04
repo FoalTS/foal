02573b0181b76c62fc23825ea06768cce97bfc0c
Loïc Poullain <loic.poullain@centraliens.net>
2022-06-04 11:38:49 +0200

    [CLI] Remove comments from generated files

--- packages/cli/src/generate/specs/app/src/scripts/create-user.ts
+++ packages/cli/src/generate/specs/app/src/scripts/create-user.ts
@@ -1,5 +1,4 @@
 // 3p
-// import { hashPassword } from '@foal/core';
 import { createConnection } from 'typeorm';
 
 // App
@@ -8,20 +7,19 @@ import { User } from '../app/entities';
 export const schema = {
   additionalProperties: false,
   properties: {
-    // email: { type: 'string', format: 'email' },
-    // password: { type: 'string' },
+
   },
-  required: [ /* 'email', 'password' */ ],
+  required: [
+
+  ],
   type: 'object',
 };
 
-export async function main(/*args*/) {
+export async function main() {
   const connection = await createConnection();
 
   try {
     const user = new User();
-    // user.email = args.email;
-    // user.password = await hashPassword(args.password);
 
     console.log(await user.save());
   } catch (error: any) {
