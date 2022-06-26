f5d3453b9d3ca975dc0a9fe0f975ffe54540fe04
Loïc Poullain <loic.poullain@centraliens.net>
2022-06-26 13:35:04 +0200

    Make "g entity mongodb" use BaseEntity

--- packages/cli/src/generate/specs/entity/test-foo-bar.entity.mongodb.ts
+++ packages/cli/src/generate/specs/entity/test-foo-bar.entity.mongodb.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, ObjectID, ObjectIdColumn } from 'typeorm';
 
 @Entity()
-export class TestFooBar {
+export class TestFooBar extends BaseEntity {
 
   @ObjectIdColumn()
   id: ObjectID;
