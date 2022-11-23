91bf8e28d7f076b8b0da6f3f9f9b6d3a413b95bd
Loïc Poullain <loic.poullain@centraliens.net>
2022-11-23 07:56:40 +0100

    [CLI] Add logging in `foal upgrade`

--- packages/cli/src/generate/generators/upgrade/upgrade.ts
+++ packages/cli/src/generate/generators/upgrade/upgrade.ts
@@ -3,6 +3,7 @@ import * as fetch from 'node-fetch';
 
 // FoalTS
 import { FileSystem } from '../../file-system';
+import { logger } from '../../utils';
 
 export async function getLatestVersion(): Promise<string> {
   const response = await fetch('https://registry.npmjs.org/@foal/core/latest');
@@ -12,6 +13,9 @@ export async function getLatestVersion(): Promise<string> {
 
 export async function upgrade(version?: string, options: { getLatestVersion: typeof getLatestVersion } = { getLatestVersion }): Promise<void> {
   version ??= await options.getLatestVersion();
+
+  logger.log();
+  logger.log(`  Upgrading @foal/* dependencies to version ${version}...`);
 
   const fs = new FileSystem();
 
