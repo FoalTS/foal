f4dd9f8b6baa4b91348349cf7e68e74c6e971777
Loïc Poullain <loic.poullain@centraliens.net>
2022-11-23 07:18:04 +0100

    [CLI] Implement "foal upgrade" with a specific version

--- packages/cli/src/generate/generators/upgrade/upgrade.ts
+++ packages/cli/src/generate/generators/upgrade/upgrade.ts
@@ -1,3 +1,23 @@
+// FoalTS
+import { FileSystem } from '../../file-system';
+
 export async function upgrade(version?: string): Promise<void> {
-  // ...
+  // Temp
+  version ??= '0.0.0';
+
+  const fs = new FileSystem();
+
+  const dependencies = fs.getProjectDependencies();
+  for (const dependency of dependencies) {
+    if (dependency.name.startsWith('@foal/')) {
+      fs.setOrUpdateProjectDependency(dependency.name, version);
+    }
+  }
+
+  const devDependencies = fs.getProjectDevDependencies();
+  for (const devDependency of devDependencies) {
+    if (devDependency.name.startsWith('@foal/')) {
+      fs.setOrUpdateProjectDevDependency(devDependency.name, version);
+    }
+  }
 }
