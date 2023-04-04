73affdcac127fbf0de87af2ac7be39de0b9a73f2
gh-actions <actions@gihub.com>
2023-04-04 18:16:38 +0000

    Deploy website - based on 97c83dee1cdb84979885d696f698662cc5af5d55

---
+++ es/assets/js/150a2a2f.3d9bc39e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4293],{30910:e=>{e.exports=JSON.parse('{"permalink":"/es/blog/tags/release/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":18,"previousPage":"/es/blog/tags/release","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ fr/assets/js/6a84c208.34aecfa8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4999],{36730:e=>{e.exports=JSON.parse('{"permalink":"/fr/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":19,"previousPage":"/fr/blog","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
