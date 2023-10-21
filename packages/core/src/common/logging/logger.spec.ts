0a82607483ce18482cee11b4439d3c8d8f436e8c
Loïc Poullain <loic.poullain@centraliens.net>
2023-10-21 17:01:51 +0200

    Add Logger.log

--- packages/core/src/common/logging/logger.spec.ts
+++ packages/core/src/common/logging/logger.spec.ts
@@ -3,102 +3,101 @@ import { strictEqual } from 'assert';
 import { mock } from 'node:test';
 
 // FoalTS
-import { Logger, log } from './logger';
+import { Logger } from './logger';
 import { Config } from '../../core';
 
-describe('log', () => {
+describe('Logger', () => {
+
   afterEach(() => {
     mock.reset();
     Config.remove('settings.logger.format');
     Config.remove('settings.logger.logLevel');
-  });
-
-  context('given the configuration "settings.logger.format" is NOT defined', () => {
-    it('should log the message to a raw text.', () => {
-      const consoleMock = mock.method(console, 'log', () => {});
-
-      log('error', 'Hello world', {});
-
-      strictEqual(consoleMock.mock.callCount(), 1);
-
-      const loggedMessage = consoleMock.mock.calls[0].arguments[0];
-
-      strictEqual(loggedMessage.includes('[ERROR]'), true);
-    })
-  });
-
-  context('given the configuration "settings.logger.format" is "json"', () => {
-    it('should log the message to a JSON.', () => {
-      Config.set('settings.logger.format', 'json');
-
-      const consoleMock = mock.method(console, 'log', () => {});
-
-      log('error', 'Hello world', {});
-
-      strictEqual(consoleMock.mock.callCount(), 1);
-
-      const loggedMessage = consoleMock.mock.calls[0].arguments[0];
-      const json = JSON.parse(loggedMessage);
-
-      strictEqual(json.level, 'error');
-    });
-  });
-
-  context('given the configuration "settings.logger.format" is "none"', () => {
-    it('should log nothing.', () => {
-      Config.set('settings.logger.format', 'none');
-
-      const consoleMock = mock.method(console, 'log', () => {});
-
-      log('error', 'Hello world', {});
-
-      strictEqual(consoleMock.mock.callCount(), 0);
-    });
-  });
-
-  context('given the configuration "settings.logger.logLevel" is NOT defined', () => {
-    it('should log messages based on an "INFO" log level', () => {
-      const consoleMock = mock.method(console, 'log', () => {});
-
-      log('info', 'Hello world', {});
-      strictEqual(consoleMock.mock.callCount(), 1);
-
-      consoleMock.mock.resetCalls();
-
-      log('debug', 'Hello world', {});
-      strictEqual(consoleMock.mock.callCount(), 0);
-    });
-  });
-
-  context('given the configuration "settings.logger.logLevel" is "warn"', () => {
-    it('should log messages based on a "WARN" log level', () => {
-      Config.set('settings.logger.logLevel', 'warn');
-
-      const consoleMock = mock.method(console, 'log', () => {});
-
-      log('warn', 'Hello world', {});
-      strictEqual(consoleMock.mock.callCount(), 1);
-
-      consoleMock.mock.resetCalls();
-
-      log('info', 'Hello world', {});
-      strictEqual(consoleMock.mock.callCount(), 0);
-    });
-  });
-});
-
-describe('Logger', () => {
-
-  beforeEach(() => {
-    Config.set('settings.logger.logLevel', 'debug');
-  });
-
-  afterEach(() => {
-    mock.reset();
-    Config.remove('settings.logger.logLevel');
   })
 
+  describe('has a "log" method that', () => {
+
+    context('given the configuration "settings.logger.format" is NOT defined', () => {
+      it('should log the message to a raw text.', () => {
+        const consoleMock = mock.method(console, 'log', () => {});
+
+        const logger = new Logger();
+        logger.log('error', 'Hello world', {});
+
+        strictEqual(consoleMock.mock.callCount(), 1);
+
+        const loggedMessage = consoleMock.mock.calls[0].arguments[0];
+
+        strictEqual(loggedMessage.includes('[ERROR]'), true);
+      })
+    });
+
+    context('given the configuration "settings.logger.format" is "json"', () => {
+      it('should log the message to a JSON.', () => {
+        Config.set('settings.logger.format', 'json');
+
+        const consoleMock = mock.method(console, 'log', () => {});
+
+        const logger = new Logger();
+        logger.log('error', 'Hello world', {});
+
+        strictEqual(consoleMock.mock.callCount(), 1);
+
+        const loggedMessage = consoleMock.mock.calls[0].arguments[0];
+        const json = JSON.parse(loggedMessage);
+
+        strictEqual(json.level, 'error');
+      });
+    });
+
+    context('given the configuration "settings.logger.format" is "none"', () => {
+      it('should log nothing.', () => {
+        Config.set('settings.logger.format', 'none');
+
+        const consoleMock = mock.method(console, 'log', () => {});
+
+        const logger = new Logger();
+        logger.log('error', 'Hello world', {});
+
+        strictEqual(consoleMock.mock.callCount(), 0);
+      });
+    });
+
+    context('given the configuration "settings.logger.logLevel" is NOT defined', () => {
+      it('should log messages based on an "INFO" log level', () => {
+        const consoleMock = mock.method(console, 'log', () => {});
+
+        const logger = new Logger();
+        logger.log('info', 'Hello world', {});
+        strictEqual(consoleMock.mock.callCount(), 1);
+
+        consoleMock.mock.resetCalls();
+
+        logger.log('debug', 'Hello world', {});
+        strictEqual(consoleMock.mock.callCount(), 0);
+      });
+    });
+
+    context('given the configuration "settings.logger.logLevel" is "warn"', () => {
+      it('should log messages based on a "WARN" log level', () => {
+        Config.set('settings.logger.logLevel', 'warn');
+
+        const consoleMock = mock.method(console, 'log', () => {});
+
+        const logger = new Logger();
+        logger.log('warn', 'Hello world', {});
+        strictEqual(consoleMock.mock.callCount(), 1);
+
+        consoleMock.mock.resetCalls();
+
+        logger.log('info', 'Hello world', {});
+        strictEqual(consoleMock.mock.callCount(), 0);
+      });
+    });
+  });
+
   it('has a debug(...args) method which is an alias for log("debug", ...args)', () => {
+    Config.set('settings.logger.logLevel', 'debug');
+
     const logger = new Logger();
 
     const consoleMock = mock.method(console, 'log', () => {});
--- packages/core/src/common/logging/logger.ts
+++ packages/core/src/common/logging/logger.ts
@@ -1,47 +1,47 @@
 import { Config } from '../../core';
 import { Level, formatMessage, shouldLog } from './logger.utils';
 
-export function log(
-  level: Level,
-  message: string,
-  params: { [name: string]: any }
-): void {
-  const format = Config.get('settings.logger.format', 'string', 'raw');
-  if (format === 'none') {
-    return;
+export class Logger {
+  log(
+    level: Level,
+    message: string,
+    params: { [name: string]: any } = {}
+  ): void {
+    const format = Config.get('settings.logger.format', 'string', 'raw');
+    if (format === 'none') {
+      return;
+    }
+
+    const configLogLevel = Config.get('settings.logger.logLevel', 'string', 'info');
+    if (!shouldLog(level, configLogLevel)) {
+      return;
+    };
+
+    const now = new Date();
+    const formattedMessage = formatMessage(
+      level,
+      message,
+      params,
+      format,
+      now,
+    );
+
+    console.log(formattedMessage);
   }
 
-  const configLogLevel = Config.get('settings.logger.logLevel', 'string', 'info');
-  if (!shouldLog(level, configLogLevel)) {
-    return;
-  };
-
-  const now = new Date();
-  const formattedMessage = formatMessage(
-    level,
-    message,
-    params,
-    format,
-    now,
-  );
-
-  console.log(formattedMessage);
-}
-
-export class Logger {
   debug(message: string, params: { error?: Error, [name: string]: any } = {}): void {
-    log('debug', message, params);
+    this.log('debug', message, params);
   }
 
   info(message: string, params: { error?: Error, [name: string]: any } = {}): void {
-    log('info', message, params);
+    this.log('info', message, params);
   }
 
   warn(message: string, params: { error?: Error, [name: string]: any } = {}): void {
-    log('warn', message, params);
+    this.log('warn', message, params);
   }
 
   error(message: string, params: { error?: Error, [name: string]: any } = {}): void {
-    log('error', message, params);
+    this.log('error', message, params);
   }
 }
