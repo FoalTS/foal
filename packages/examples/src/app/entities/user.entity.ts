548eb0aaefedd9dadbba85b5e22929250a624e82
Loïc Poullain <loic.poullain@centraliens.net>
2023-10-25 12:17:32 +0200

    Fix linting

--- packages/examples/src/app/entities/user.entity.ts
+++ packages/examples/src/app/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 import { hashPassword } from '@foal/core';
 import { UserWithPermissions } from '@foal/typeorm';
