4578080e204e34e13b3240b5f518b831a168a4eb
Loïc Poullain <loic.poullain@protonmail.com>
2023-12-04 22:55:12 +0100

    Use fs native API with promises

--- packages/cli/src/rmdir/rmdir.util.ts
+++ packages/cli/src/rmdir/rmdir.util.ts
@@ -1,7 +1,5 @@
 // std
-import { readdir, rmdir as fsRmdir, stat, unlink } from 'fs';
-import { join } from 'path';
-import { promisify } from 'util';
+import { rm, readdir } from 'node:fs/promises';
 
 /**
  * Remove a directory and all its contents, including any subdirectories and files.
@@ -13,7 +11,7 @@ import { promisify } from 'util';
 export async function rmdir(path: string): Promise<void> {
   let contents: string[];
   try {
-    contents = await promisify(readdir)(path);
+    contents = await readdir(path);
   } catch (error: any) {
     if (error.code === 'ENOENT') {
       return;
@@ -21,16 +19,5 @@ export async function rmdir(path: string): Promise<void> {
     throw error;
   }
 
-  await Promise.all(contents.map(content => {
-    const subPath = join(path, content);
-    return promisify(stat)(subPath)
-      .then(stats => {
-        if (stats.isDirectory()) {
-          return rmdir(subPath);
-        }
-        return promisify(unlink)(subPath);
-      });
-  }));
-
-  await promisify(fsRmdir)(path);
+  await rm(path, { recursive: true });
 }
