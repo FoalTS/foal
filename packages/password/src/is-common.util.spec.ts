58123fb0458b90b84a07c4e5643187663d4d6b85
Lucho <programando.soft@gmail.com>
2024-04-26 07:15:29 -0300

    Lint

--- packages/password/src/is-common.util.spec.ts
+++ packages/password/src/is-common.util.spec.ts
@@ -6,54 +6,40 @@ import { clearCommonPasswordsCache, isCommon } from './is-common.util';
 
 describe('isCommon util', () => {
   describe('isCommon', () => {
-
     describe('default datasource', () => {
-  
       it('should return true if the given password is part of the 10000 most common passwords.', async () => {
         strictEqual(await isCommon('12345'), true);
       });
-    
       it('should return false if the given password is not part of the 10000 most common passwords.', async () => {
         strictEqual(await isCommon('a bird in the sky'), false);
       });
-  
     });
   
     describe('10k datasource', () => {
-  
       it('should return true if the given password is part of the 100000 most common passwords.', async () => {
         strictEqual(await isCommon('12345', 'TenMillionListTop10k'), true);
       });
-    
       it('should return false if the given password is not part of the 100000 most common passwords.', async () => {
         strictEqual(await isCommon('a bird in the sky', 'TenMillionListTop10k'), false);
       });
-  
     });
   
     describe('100k datasource', () => {
-  
       it('should return true if the given password is part of the 100000 most common passwords.', async () => {
         strictEqual(await isCommon('Cinder', 'TenMillionListTop100k'), true);
       });
-    
       it('should return false if the given password is not part of the 100000 most common passwords.', async () => {
         strictEqual(await isCommon('a bird in the sky', 'TenMillionListTop100k'), false);
       });
-  
     });
-  
   });
 
   describe('clearCommonPasswordsCache', () => {
-
     it('should not fail if it is empty',  () => {
       clearCommonPasswordsCache();
     });
-  
     it('should not fail if it is full', async () => {
       await isCommon('12345', 'TenMillionListTop10k');
-      
       clearCommonPasswordsCache();
     });
   });
