3d5c845944e6a34ae77a5720a73dbbf4f12fa003
Loïc Poullain <loic.poullain@protonmail.com>
2024-07-27 16:16:40 +0200

    Deprecate displayServerURL

--- packages/core/src/common/utils/display-server-url.util.ts
+++ packages/core/src/common/utils/display-server-url.util.ts
@@ -1,3 +1,10 @@
+/**
+ * Display server URL in console
+ *
+ * This function is deprecated. Use "services.get(Logger).info(`Listening on port ${port}...`)" instead.
+ *
+ * @deprecated
+ */
 export function displayServerURL(port: number, log = console.log) {
   log();
   log('\x1b[32m -------------------------------------- \x1b[0m');
