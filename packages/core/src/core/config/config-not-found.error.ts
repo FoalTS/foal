3afa6d97e8075e68d88807f1ebaea5792d5579f0
Loïc Poullain <loic.poullain@protonmail.com>
2024-08-03 20:14:36 +0200

    Fix linting

--- packages/core/src/core/config/config-not-found.error.ts
+++ packages/core/src/core/config/config-not-found.error.ts
@@ -3,7 +3,7 @@ export class ConfigNotFoundError extends Error {
 
   constructor(readonly key: string, readonly msg?: string) {
     super();
- 
+
     this.message = `No value found for the configuration key "${key}".${msg === undefined ? '' : ` ${msg}`}`;
   }
 
